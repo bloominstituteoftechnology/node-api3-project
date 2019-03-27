7422cd0076635d770ef7af5e77e3a1e35957214f
Luis Hernandez <luishrd@gmail.com>
2019-03-27 08:01:04 -0600

    fixes typos on seeds

--- data/seeds/02-users.js
+++ data/seeds/02-users.js
@@ -1,13 +1,13 @@
 exports.seed = function(knex, Promise) {
   return knex('users').insert([
-    { name: 'Frodo Baggings' }, // 1
+    { name: 'Frodo Baggins' }, // 1
     { name: 'Samwise Gamgee' }, // 2
     { name: 'Meriadoc Brandybuck' }, // 3
     { name: 'Peregrin Took' }, // 4
     { name: 'Mithrandir' }, // 5
     { name: 'Boromir' }, // 6
     { name: 'Legolas' }, // 7
-    { name: 'Gimly' }, // 8
+    { name: 'Gimli' }, // 8
     { name: 'Aragorn' }, // 9
   ]);
 };
