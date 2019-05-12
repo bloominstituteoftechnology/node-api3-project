32f29a5ee71417f9aef6e3b466ff194dfb7791a5
Diandra Ryan-Mas <dryanmas@Diandras-MacBook-Air.local>
2019-05-12 09:18:00 -0700

    defined endpoints

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,7 +2,31 @@ const express = 'express';
 
 const router = express.Router();
 
+router.post('/', (req, res) => {
+
+});
+
+router.post('/:id/posts', (req, res) => {
+
+});
+
 router.get('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.get('/:id/posts', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
 
 });
 
