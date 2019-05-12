6d3d313b94f161273e0b2d32ba6d37ad2e7613e5
Diandra Ryan-Mas <dryanmas@Diandras-MacBook-Air.local>
2019-05-12 09:14:53 -0700

    added middleware functions

---
+++ routers/userRouter.js
@@ -0,0 +1,23 @@
+const express = 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+});
+
+//custom middleware
+
+function validateUserId(req, res, next) {
+
+};
+
+function validateUser(req, res, next) {
+
+};
+
+function validatePost(req, res, next) {
+
+};
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = 'express';
+
+const server = express();
+
+server.get('/', (req, res) => {
+
+});
+
+//custom middleware
+
+function logger(req, res, next) {
+
+};
+
+module.exports = server;
