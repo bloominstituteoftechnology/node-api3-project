42b5ad362c1cf2c0437e45cb37da03db83485d9a
Luis Hernandez <luishrd@gmail.com>
2019-10-30 14:40:42 -0600

    fixes express import

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,27 @@
-const express = 'express';
+const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+  // do your magic!
 });
 
 router.get('/:id', (req, res) => {
-
+  // do your magic!
 });
 
 router.delete('/:id', (req, res) => {
-
+  // do your magic!
 });
 
 router.put('/:id', (req, res) => {
-
+  // do your magic!
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-
-};
+  // do your magic!
+}
 
 module.exports = router;
