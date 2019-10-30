3027986fb4c6561543c4cc30808b30a607d4372c
Luis Hernandez <luishrd@gmail.com>
2019-10-30 16:17:24 -0600

    fixes express import

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function logger(req, res, next) {}
 
 module.exports = server;
