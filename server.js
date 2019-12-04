1d4672a95d83a3ef21567210d3a2056b1d0b878a
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2019-12-04 13:35:43 -0800

    testing

--- server.js
+++ server.js
@@ -4,8 +4,24 @@ const server = express();
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
-});
 
+
+const { rawHeaders, httpVersion, method, socket, url } = req;
+const { remoteAddress, remoteFamily } = socket;
+
+console.log(
+  json.stringify({
+    timestamp: Date.now(),
+    rawHeaders,
+    httpVersion,
+    method,
+    remoteAddress,
+    remoteFamily,
+    url
+  })
+);
+res.end()
+});
 //custom middleware
 
 function logger(req, res, next) {}
