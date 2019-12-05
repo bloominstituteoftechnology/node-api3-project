614f7f177c815fd0079bfc33d860aed0177c4fc4
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2019-12-04 16:44:31 -0800

    built get/ and get by id

--- server.js
+++ server.js
@@ -1,29 +1,41 @@
 const express = require('express');
-
+const helmet = require("helmet")
 const server = express();
+const userRoutes = require("./users/userRouter")
 
+server.use(helmet());
+
+
+// server.get("/echo", (req, res)=> {
+//   res.send(req.headers);
+// })
+// //custom middleware
+
+// function logger(req, res, next) {
+//   console.log(`${req.method} to ${req.originalUrl}`)// allows the req to continue to the next middleware or route handler
+// }
+
+const logger = (req, res, next) => {
+  console.log(`${req.method} to ${req.originalUrl} at ${new Date()}`);
+  next();
+}
+server.use(express.json());
+server.use(logger)
+server.use("/api/users", userRoutes)
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
-
-
-const { rawHeaders, httpVersion, method, socket, url } = req;
-const { remoteAddress, remoteFamily } = socket;
-
-console.log(
-  json.stringify({
-    timestamp: Date.now(),
-    rawHeaders,
-    httpVersion,
-    method,
-    remoteAddress,
-    remoteFamily,
-    url
-  })
-);
-res.end()
 });
-//custom middleware
+// function logger(req, res, next) {
+//   const { method, url } = req;
 
-function logger(req, res, next) {}
+// console.log(
+//   JSON.stringify({
+//     timestamp: Date.now(),
+//     method,
+//     url
+//   })
+// );
+// // res.end()}
+
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const Users = require("./userDb")
+
 const router = express.Router();
 
 router.post('/', (req, res) => {
@@ -10,12 +12,21 @@ router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get('/', checkRole("admin"), (req, res) => {
+  Users
+  .get()
+  .then(users => {
+    res.status(200).json(users);
+  })
+  .catch(() => {
+    res.status(500).json({message: "cant get the users from the data base sorry buddy"})
+  }) 
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id',validateUserId, (req, res) => {
+  res
+    .status(200)
+    .json(req.user);
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -31,9 +42,32 @@ router.put('/:id', (req, res) => {
 });
 
 //custom middleware
+function checkRole(role) {
+  return function(req, res, next) {
+    if (role && role === req.headers.role) {
+      next();
+    }else {
+      res.status(403).json({message: "cant touch that you not admin, or an agent"})
+    }
+  }
+}
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const userId = req.params.id || req.bod.user_id;
+  Users
+  .getById(userId)
+  .then(user => {
+    if (user) {
+      req.user = user;
+    return next()
+    }else {
+      res.status(400)
+      .jsonn({message: "that is not a valid id"})
+    }
+  })
+  .catch(() => {
+    res.status(500).json({message: "cant find that user in our data"})
+  })
 }
 
 function validateUser(req, res, next) {
