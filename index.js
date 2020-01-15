677c2f9bc4b78a1002c9b1dde226c467b2872aa4
jarrodskahill <jarrodskahill@gmail.com>
2020-01-15 13:47:17 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // code away!
+const server = require('./server')
+const port = 8000
+server.listen(port, () => console.log(`\n ** api on port: ${port} ** \n`))
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
+const postrouter = require('./posts/postRouter');
+const userrouter = require('./users/userRouter')
 const server = express();
+server.use(express.json());
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +11,8 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+
+function logger(req, res, next) {console.log(`${req.method} ${req.originalUrl} at ${new Date().toISOString()}`);
+  next();}
 
 module.exports = server;
