952ad7cf2e8a47a59569d3604bb58d5f55802c5d
Aasa-Christian <aasachristian@yahoo.com>
2020-01-16 16:16:35 -0500

    downloaded depends, created index and server.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code away!
+const server = require('./server');
+
+const port = 4000
+
+server.listen(port, () => {
+    console.log(`\n *** Server Running on http://localhost:${port} *** \n`);
+})
--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const server = express();
 
+const userRouter = require('./users/userRouter')
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+server.use(express.json());
+server.use('/api/user', userRouter)
 
 //custom middleware
 
