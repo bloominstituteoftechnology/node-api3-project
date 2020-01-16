f339e865f7270613fa866661e28c36d74898a741
Aasa-Christian <aasachristian@yahoo.com>
2020-01-16 16:59:21 -0500

    created CRUD routs for user router

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,32 +2,81 @@ const express = require('express');
 
 const router = express.Router();
 
+const db = require('./userDb')
+
+const postDB = require('../posts/postDb')
+
 router.post('/', (req, res) => {
-  // do your magic!
+  db.insert(req.body)
+  .then(hubs => {
+    res.status(200).json(hubs)
+    console.log(hubs)
+  })
+  .catch(error => {
+    res.status(500).json({ error: "The user was not added"})
+  })
 });
 
 router.post('/:id/posts', (req, res) => {
-  // do your magic!
+  postDB.update(req.params.id, req.body)
+  .then(hubs => {
+    res.status(200).json(hubs)
+    console.log(hubs)
+  })
+  .catch(error => {
+    res.status(500).json({ error: "The post was not updated"})
+  })
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  db.get()
+  .then(hubs => {
+    res.status(200).json(hubs)
+  })
+  .catch(error => {
+    res.status(500).json({ error: "The user information could not be returned"})
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  db.getById(req.params.id)
+  .then(hubs => {
+    res.status(200).json(hubs)
+  })
+  .catch(error => {
+    res.status(500).json({ error: "The user information could not be returned"})
+  })
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+  postDB.getById(req.params.id)
+  .then(hubs => {
+    res.status(200).json(hubs)
+  })
+  .catch(error => {
+    res.status(500).json({ error: "The post information could not be returned"})
+  })
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  db.remove(req.params.id)
+  .then(hubs => {
+    res.status(200).json(hubs)
+  })
+  .catch(error => {
+    res.status(500).json({ error: "The user information could not be removed"})
+  })
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  db.update(req.params.id ,req.body)
+  .then(hubs => {
+    res.status(200).json(hubs)
+    console.log(hubs)
+  })
+  .catch(error => {
+    res.status(500).json({ error: "The user was not updated"})
+  })
 });
 
 //custom middleware
