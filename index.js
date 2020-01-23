63305829de930cdfb359ed1ecb2ba715a79e6ea3
Mohd Raza <noorinfotechinc@gmail.com>
2020-01-23 08:43:30 -0600

    env setup

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 // code away!
 
 const server = require("./server.js");
-const port = 3100;
+const port = process.env.PORT || 3100;
 
 server.listen(port, () => {
   console.log(`*** listening on port ${port}`);
