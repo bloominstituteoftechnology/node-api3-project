04fbc9a9f8dfbada934f9ad741ef63cc0fbf7abe
Toniddarden1 <Toniddarden@gmail.com>
2020-01-23 20:36:30 -0500

    mvp

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,72 @@
 const express = require('express');
-
+const posts = require('./postDb');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  posts
+    .get()
+    .then(data => res.json(data))
+    .catch(err => res.status(404).json({ message: "could not find all posts", err }));
+
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId(), (req, res) => {
+  posts.getById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({message: 'could not find posts with this ID', err}))
+  });
+
+
+
+router.delete('/:id', validatePostId(), (req, res) => {
+  posts.remove(req.params.id)
+  .then(post => {
+    res.status(200).json({message:`post has been deleted`})
+  })
+  .catch(err => res.status(404).json({errorMessage: `cannot delete post`, err}))
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId(), validatePost(), (req, res) => {
+  posts
+    .update(req.params.id, req.text)
+    .then(data => res.json(data))
+    .catch(err => res.status(404).json({ message: "could not update post", err }));
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-  // do your magic!
+function validatePost() {
+  return (req, res, next) => {
+    resource = {
+      text: req.body.text
+    }
+
+    if(!req.body.text){
+      return res.status(404).json({errorMessage: `missing post data `})
+    } else {
+      req.text = resource
+      next()
+    }
+  }
+}
+
+function validatePostId() {
+  return (req, res, next) => {
+ 
+  posts.getById(req.params.id)
+    .then(post => {
+      if (post) {
+        req.post = post;
+        next();
+      } else {
+        res.status(400).json({message: `post ID not found `})
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'server error, cannot find post', err });
+    })}
+  
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 const express = require('express');
-
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
+
+// const helmet = require('helmet');
+// const morgan = require('morgan');
+
+server.use(express.json());
+// server.use(helmet());
+// server.use(morgan('dev'));
+server.use(logger);
+server.use(addName);
+
+server.use('/users', userRouter);
+server.use('/posts', postRouter);
+
+
+server.get('/', (req, res, next) => {
+  const nameInsert = (req.name) ? `${req.name}` : "";
+  res.send(`<h2>Let's write some middleware ${nameInsert}!</h2>`);
 });
+
+
 
 //custom middleware
 
-function logger(req, res, next) {}
+function addName(req, res, next) {
+  req.name = req.name || 'Toni';
+  next();
+}
+
+function logger(req, res, next) {
+  req.time = Date.now();
+  console.log(`${req.method} to ${req.originalUrl} made at ${req.requestTime}`);
+  next();
+}
 
 module.exports = server;
