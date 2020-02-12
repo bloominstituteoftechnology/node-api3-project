ccd0660af9941fd9212de6c9c6d8d2bdc4f1f9d1
wesley.moody <wgm3190@gmail.com>
2020-02-12 14:55:13 -0600

    initial commit working on middleware

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require('express'); //  importing a CommonJS module
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const postRouter = require('./posts/postRouter');
 
 const server = express();
+
+function logger(req, res, next) {
+  console.log(`${req.method} Request to ${req.originalUrl} `);
+  next();
+}
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+//global middleware
+server.use(express.json()); //  built-in MW
 //custom middleware
+server.use(helmet());
+
 
 function logger(req, res, next) {}
 
