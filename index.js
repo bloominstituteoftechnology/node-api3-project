1716fc4ed6014467998cfae9658cbe03cdd69b7e
wesley.moody <wgm3190@gmail.com>
2020-02-12 15:36:48 -0600

    index.js server running on 7000

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require('./server');
+
+server.listen(7000, () => {
+  console.log('\n* Server Running on http://localhost:7000 *\n');
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,32 +1,32 @@
-const express = require('express');
-
+const express = require("express");
+const Db = require("./userDb");
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
