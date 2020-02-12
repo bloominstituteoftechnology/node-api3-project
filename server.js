b6cd8f254c89487190f260d91b5458c95f123b98
Sierra Curtis <sierracurtis91@gmail.com>
2020-02-12 18:52:29 -0500

    get and post users is working

--- server.js
+++ server.js
@@ -2,12 +2,27 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
+const postRouter = require('./posts/postRouter.js');
+
+const userRouter = require('./users/userRouter.js');
+
+server.use(logger);
+
+server.use(express.json());
+
+server.use('/post', postRouter);
+
+server.use('/users', userRouter)
+
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} Request to ${req.originalUrl}`);
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,20 @@
 const express = require('express');
 
+const Users = require('./userDb');
+// const Posts = require('../posts/postDb')
+
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // do your magic!
+  Users
+  .insert(req.body)
+  .then(user => {
+    res.status(200).json(user)
+  })
+  .catch(err => {
+    res.status(500).json({ error: 'The was an error while saving the user to the database' })
+  })
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -12,6 +23,12 @@ router.post('/:id/posts', (req, res) => {
 
 router.get('/', (req, res) => {
   // do your magic!
+  Users
+  .get()
+  .then(user => {
+    res.status(200).json(user)
+  })
+  .catch(err => res.status(404).json({ message: "could not find users" }));
 });
 
 router.get('/:id', (req, res) => {
@@ -34,14 +51,43 @@ router.put('/:id', (req, res) => {
 
 function validateUserId(req, res, next) {
   // do your magic!
+  Users.getById(req.params.id)
+  .then(user => {
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(400).json({ message: 'invalid user id' })
+    }
+  })
+  .catch(err  => {
+    res.status(500).json({ error: 'The user information could not be retrieved'})
+  })
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  console.log(req)
+  const body = req.body;
+    if (!body) {
+      res.status(400).json({ message: 'missing user data' })
+    } else if (!body.name) {
+    res.status(400).json({ message: 'missing required name field' })
+  }
+  next();
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  if (req.body) {
+    if (!req.body.text) {
+      res.status(400).json({ message: 'missing required text field' })
+    }
+  } else {
+    res.status(400).json({ message: 'missing post' })
+  }
+  console.log('Post validated');
+  next();
 }
 
 module.exports = router;
