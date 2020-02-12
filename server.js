78d58ae224ebd121f458198d8fbf7ddb943c6188
wesley.moody <wgm3190@gmail.com>
2020-02-12 15:31:01 -0600

    built out server.js

--- server.js
+++ server.js
@@ -1,26 +1,70 @@
-const express = require('express'); //  importing a CommonJS module
-const helmet = require('helmet');
-const morgan = require('morgan');
+//  importing a CommonJS module
+const express = require("express");
+const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
 
-const postRouter = require('./posts/postRouter');
+//  importing 3rd party MW
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const server = express();
+//global middleware
+server.use(express.json()); //  built-in MW
 
+server.use(helmet());
+server.use(morgan);
+
+//  custom middleware
 function logger(req, res, next) {
-  console.log(`${req.method} Request to ${req.originalUrl} `);
+  console.log(
+    `${req.method} Request to ${
+      req.originalUrl
+    } at ${new Date().toISOString()} `
+  );
   next();
 }
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+function validateUserId(req, res, next) {
+  db.getById(req.params.id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(500).json({ message: "No user exists with specified ID" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "ID required" });
+    });
 
-//global middleware
-server.use(express.json()); //  built-in MW
-//custom middleware
-server.use(helmet());
+  function validateUser(req, res, next) {
+    if (req.body) {
+      if (req.body.name) {
+        next();
+      } else {
+        res.status(400).json({ message: "Missing Name" });
+      }
+    } else {
+      res.status(400).json({ message: "Missing User Data" });
+    }
+  }
 
+  function validatePost(req, res, next) {
+    if (req.body) {
+      if (req.body.text) {
+        next();
+      } else {
+        res.status(400).json({ message: "Missing required text field" });
+      }
+    } else {
+      res.status(400).json({ message: "Missing post data" });
+    }
+  }
+}
 
-function logger(req, res, next) {}
+server.use(logger);
+server.use("./api/user", userRouter);
+server.use("./api/post", postRouter);
 
 module.exports = server;
