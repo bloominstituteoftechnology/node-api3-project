74cf9019c24d447549a84f52028648c301dd20f5
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-12 14:58:46 -0600

    logger works

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const postRouter = require('./posts/postRouter');
 
 const server = express();
 server.use(express.json());
+server.use(logger);
 
 server.use('/api/users', userRouter);
 server.use('/api/users/posts', postRouter);
@@ -15,3 +16,9 @@ server.get('/', (req, res) =>
 
 const PORT = 5000;
 server.listen(PORT, () => console.log(`Server is running on localhost:${PORT}`));
+
+function logger(req, res, next)
+{
+    console.log(`[${req.method}] localhost:${PORT}${req.url} ${new Date}`)
+    next();
+}
