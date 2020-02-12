cef302faa24628d7235c83d4c953f9e2a0204853
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-12 14:50:10 -0600

    packages updated and server is running

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code away!
+const express = require('express');
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
+
+const server = express();
+server.use(express.json());
+
+server.use('/api/users', userRouter);
+server.use('/api/users/posts', postRouter);
+
+server.get('/', (req, res) =>
+{
+    res.json({message: 'api is working!!'});
+})
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Server is running on localhost:${PORT}`));
