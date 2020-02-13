0381fe59f5712ed17c3413d6a99cf1888c2e35d6
Timothy <timgill987@gmail.com>
2020-02-13 14:34:29 -0500

    all done

--- data/middleware/validateUserId.js
+++ data/middleware/validateUserId.js
@@ -4,7 +4,7 @@ function validateUserId(req, res, next) {
   const { id } = req.params;
 
   if (!id || isNaN(parseInt(id))) {
-    status(400).json({ message: "Please provide a valid id" });
+    res.status(400).json({ message: "Please provide a valid id" });
   } else {
     next();
   }
