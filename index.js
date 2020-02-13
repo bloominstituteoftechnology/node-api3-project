c0ed4fb9487d715a1c7bb4386e65e24af69ec940
cholman <holman.cody@gmail.com>
2020-02-13 17:09:14 -0600

    mvp

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+
 const server = require('./server.js');
 
-const port = 5000;
+const port = process.env.PORT || 4000;
+
 server.listen(port, () => {
     console.log(`\n Server running on http://localhost:${port}`);
 })
