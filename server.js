0662cf8613b323d63ec8dd36807f0f34ef35f9d1
skraus5628 <s_read@aol.com>
2020-02-18 19:13:28 -0600

    Implemented logger and finished testing CRUD

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const logger = require("./middleware/logger")
 const userRouter = require('./users/userRouter')
 const server = express();
 
@@ -6,7 +7,9 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+
 server.use(express.json())
+server.use(logger("short"))
 server.use('/api/user', userRouter)
 //custom middleware
 
