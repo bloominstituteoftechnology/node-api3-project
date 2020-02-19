84bfc24133bf3f7bee8e678f5dc3c5699c6e8846
mandihamza <mandihamza@gmail.com>
2020-02-18 19:00:56 -0700

    Imports base server routes from /welcome-router

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express")
 
+const welcomeRouter = require("./welcome/welcome-router.js")
+
 const server = express()
 const port = 4000
 
 server.use(express.json())
 
-server.get('/', (req, res) => {
-    res.send(`<h2>Let's write some middleware!</h2>`);
-  });
+server.use("/", welcomeRouter)
 
 server.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`)
