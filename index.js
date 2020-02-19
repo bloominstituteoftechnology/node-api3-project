815f75a2628329cab50c10184833d4b38468c2fe
mandihamza <mandihamza@gmail.com>
2020-02-18 18:56:44 -0700

    Initial express server config

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code away!
+const express = require("express")
+
+const server = express()
+const port = 4000
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`);
+  });
+
+server.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`)
+})
