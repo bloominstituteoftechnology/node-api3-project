b58b1ec577b5a21813a85fdb13eadaf937746462
Ariunbold <ariun1118@gmail.com>
2020-02-19 21:45:18 -0500

    PostRouter is done

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,26 +2,64 @@ const express = require('express');
 const posts = require("./postDb")
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   // do your magic!
+  posts.get()
+    .then( post => res.json(post))
+    .catch(next)
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validatePostId(), (req, res) => {
   // do your magic!
+  res.status(200).json(req.post)
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
   // do your magic!
+  posts.remove(req.params.id)
+    .then(() => {
+      res.status(200).json({
+        message: "The post is deleted"
+      })
+    })
+    .catch(next)
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId(), (req, res, next) => {
   // do your magic!
+  posts.update(req.params.id, req.body)
+    .then(post => {
+      if(post){
+        res.status(200).json({
+          message: "Post is updated"
+        })
+      } else {
+        res.status(404).json({
+          message : "The post is not found"
+        })
+      }
+    })
+    .catch(next)
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
+function validatePostId() {
   // do your magic!
+  return (req, res, next) => {
+    posts.getById(req.params.id)
+      .then(post => {
+        if(post) {
+          req.post = post
+          next()
+        } else {
+          res.status(404).json({
+            message: "Post is not found"
+          })
+        }
+      })
+      .catch(next)
+  }
 }
 
 module.exports = router;
