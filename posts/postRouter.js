32bb6da49216125a92f75aaee9ed55eaf066caf4
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-03-11 18:24:14 -0400

    Implemented get by id method

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -8,14 +8,26 @@ router.get('/', async (req, res) => {
   try {
       let posts = await db.get()
       res.status(200).json(posts)
-  } catch (err) {
+  } catch(err) {
       console.error(err)
       res.status(500).json({ error: "The posts information could not be retrieved." })
   }
 })
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', async (req, res) => {
+  try {
+    let posts = await db.getById(req.params.id);
+    if (posts.length > 0) {
+      res.status(200).json(posts[0])
+    }
+    else {
+      res.status(400).json({error: "Could not find any posts with that id."})
+    }
+  }
+  catch(err) {
+    console.error(err)
+    res.status.apply(500).json({ error: "The post information could not be retrieved." })
+  } 
 });
 
 router.delete('/:id', (req, res) => {
