ea161e1cca6fe4e622441ce730c546ffd71f2b69
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-03-11 18:20:22 -0400

    Implemented logger and get method

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,9 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // do your magic!
-});
+const db = require('./postDb');
+
+router.get('/', async (req, res) => {
+  try {
+      let posts = await db.get()
+      res.status(200).json(posts)
+  } catch (err) {
+      console.error(err)
+      res.status(500).json({ error: "The posts information could not be retrieved." })
+  }
+})
 
 router.get('/:id', (req, res) => {
   // do your magic!
--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require('express');
 
 const server = express();
 
+const router = require('./posts/postRouter')
+
+server.use(logger)
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use('/api/posts', router)
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log("\x1b[36m", `${req.method}`, "\x1b[32m", `to ${req.originalUrl}`)
+  next()
+}
 
 module.exports = server;
