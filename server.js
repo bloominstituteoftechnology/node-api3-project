ceae80efde031f8097c9c1db2d7aa689f93af4e4
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-03-11 10:42:18 +0100

    wip

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const server = express();
 // plug in the body parsing ability
 server.use(express.json())
 
-server.get('/:id', (req, res) => {
-  res.send(`<h2>That is a nice id: ${req.params.id}</h2>`);
-})
-
 server.get('/friend', (req, res) => {
   res.send(`<h2>Hello, friend!</h2>`);
+})
+
+server.get('/:id', (req, res) => {
+  res.send(`<h2>That is a nice id: ${req.params.id}</h2>`);
 })
 
 //custom middleware
