d02cf1492361665cbd20bf899a457736e094c4d1
johnkirtley <kirtleyj16@gmail.com>
2020-03-11 17:00:43 -0700

    Completed MVP

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,74 @@
 const express = require('express');
+const posts = require('./postDb');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+	posts
+		.get()
+		.then(posts => {
+			res.status(201).json(posts);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ message: 'Could not get posts' });
+		});
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, (req, res) => {
+	// do your magic!
+	const { id } = req.params;
+	posts
+		.getById(id)
+		.then(post => {
+			res.status(201).json(post);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ message: 'Error getting ID' });
+		});
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validatePostId, (req, res) => {
+	// do your magic!
+	const { id } = req.params;
+	posts
+		.remove(id)
+		.then(post => {
+			res.status(201).json(post);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ message: 'Could not delete post' });
+		});
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId, (req, res) => {
+	// do your magic!
+	const { id } = req.params;
+	const response = req.body;
+	posts
+		.update(id, response)
+		.then(response => {
+			res.status(201).json(response);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ message: 'Could not update post' });
+		});
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+	// do your magic!
+	const { id } = req.params;
+
+	if (!id) {
+		res.status(500).json({ message: 'Please make sure you have a post ID' });
+	}
+
+	next();
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const server = express();
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
+
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+
+server.use(logger);
+
+// Routers
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+	res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+	const method = req.method;
+	const url = req.originalUrl;
+	const timeStamp = Date.now();
+
+	console.log(`\nMethod: ${method} \nUrl: ${url} \nTime: ${timeStamp}`);
+
+	next();
+}
 
 module.exports = server;
