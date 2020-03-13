e8e1483f79e41f8b64c3fc85652a467ea355e5e1
mastaryan <sizemore.ryan@gmail.com>
2020-03-13 13:55:16 -0400

    update server files for Heroku App

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-// code away!
-server = require('./server.js');
+// it's recommended to load configuration for .env as early as possible
+require('dotenv').config(); // add this line as the first thing to run1
 
-const port = 5000;
+const server = require('./api/server.js');
+
+// we'll read the port from the server environment if it is there
+// heroku will have the PORT environment variable set
+const port = process.env.PORT || 5000;
+
+// we can now use that port, if set up by heroku or read from .env or 5000 as a default if not set
 server.listen(port, () => {
-    console.log('Server listening on localhost:${port}');
-})
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
