558257fc4472ecd4b64c1110925858c556f4217d
raudelf <rawdell@outlook.com>
2020-03-17 22:45:29 -0500

    Finished the logger and implemented it

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require('express');
 
 const server = express();
 
+// Implemented Middleware
+server.use(logger);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
+// Middleware Functions
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} Request`);
+  console.log(`Endpoint URL: ${req.url}`)
+  next();
+};
 
 module.exports = server;
