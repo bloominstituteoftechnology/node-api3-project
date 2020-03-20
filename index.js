a0aa245965b9d26a5ab970070f276485e235db65
Shya Biswas <shyabiswas@gmail.com>
2020-03-19 22:47:26 -0400

    MVP

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
+require("dotenv").config();
 const app = require("./server.js")
-const port = 5000;
+
+const port = process.env.PORT||5000;
 
 app.listen(port, ()=>{
     console.log(`server running on http://localhost:${port}`)
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const cors = require("cors")
 const postRouter= require("./posts/postRouter");
 const userRouter= require("./users/userRouter")
 const server = express();
 
+server.use(cors())
 server.use(express.json())
 server.use(logger)
 server.use('/api/users', userRouter)
 server.use('/api/posts', postRouter)
 
-server.get('/', (req, res) => {
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
