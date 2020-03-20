5bf93f48a857d23f018d7df4842b06f9ce42e217
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-03-20 14:38:04 -0700

    Added message in .env

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ server.use('/api/posts', postRouter);
 server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware !</h2>
-  <p>The browser server is running fine</p>`);
+  // res.send(`<h2>Let's write some middleware !</h2>
+  // <p>The browser server is running fine</p>`);
   res.status(200).json({ message: process.env.MOTD });
 });
 
