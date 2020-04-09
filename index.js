1a87cedc7e53a996aa0bb9d1a71b891e88e74cb8
Jake Johnson <jakejjohnson@gmail.com>
2020-04-09 11:30:41 -0500

    functioning?

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+const server = require("./server.js");
+
+const port = 8000;
+
+server.listen(port, () => {
+	console.log(`server started at http://localhost:${port}`);
+});
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
+const userRoutes = require("./users/userRouter");
+const postRoutes = require("./posts/postRouter");
+const logger = require("./middleware/logger.js");
+
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
-
-//custom middleware
-
-function logger(req, res, next) {}
+// middleware
+server.use(logger);
+server.use(helmet());
+server.use(express.json());
 
 module.exports = server;
