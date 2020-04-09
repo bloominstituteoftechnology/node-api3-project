72c33eba13dcacd057487dc2eba96749bbc97ca7
Miranda Musselman <mirandamusselman@Pams-MacBook-Pro.local>
2020-04-09 10:18:48 -0600

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
-
+const helmet = require('helmet');
 const server = express();
 
-server.get('/', (req, res) => {
+const userRouter = require("./users/userRouter");
+
+// MW
+server.use(helmet());
+server.use(express.json());
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+server.use("/api/users", logger, userRouter);
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`[${Date.now()}] ${req.method} Request to ${req.originalUrl}`);
+
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,118 @@
 const express = require('express');
-
+const Data = require("./userDb");
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", validateUser, (req, res) => {
   // do your magic!
+  Data.insert(req.body)
+    .then(() => {
+      res.status(201).json(req.body);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        messge: "There was an error while saving the post to the database",
+      });
+    });
 });
 
 router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // do your magic!
+  Data.get(req.query)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving posts",
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get("/:id", validateUserId, (req, res) => {
+  console.log(req.u);
+  res.status(200).json(req.u);
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get("/:id/posts", validateUserId, (req, res) => {
   // do your magic!
+  Data.getUserPosts(req.params.id)
+    .then((posts) => {
+      if (posts) {
+        res.status(200).json(posts);
+      } else {
+        res.status(404).json({
+          message: "post not found",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error processing request",
+      });
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", validateUserId, (req, res) => {
   // do your magic!
+  Data.remove(req.params.id)
+    .then((user) => res.status(200).json(user))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "The user could not be removed" });
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", validateUserId, validateUser, (req, res) => {
   // do your magic!
+  Data.update(req.params.id, req.body)
+    .then(res.status(200).json(req.body))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Error processing request" });
+    });
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const { id } = req.params;
+  Data.getById(id)
+    .then((u) => {
+      if (u) {
+        req.u = u;
+        next();
+      } else {
+        res.status(404).json({ message: "ID not found" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "failed", err });
+    });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  const body = req.body;
+  !body || body === {}
+    ? res.status(400).json({ message: "user not included" })
+    : !body.name
+      ? res.status(400).json({ message: "missing name" })
+      : next();
 }
-
 function validatePost(req, res, next) {
-  // do your magic!
+  const body = req.body;
+  !body || body === {}
+    ? res.status(400).json({ message: "post not included" })
+    : !body.text
+      ? res.status(400).json({ message: "missing text" })
+      : next();
 }
 
 module.exports = router;
