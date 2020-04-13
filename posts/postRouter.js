ad3a61618b5204634ee398be43c755c3c6a2c523
anthony2698 <carrilloanthony2698@gmail.com>
2020-04-13 10:26:48 -0400

    mvp.

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,65 +1,93 @@
-const express = require("express");
-
-const db = require("./postDb");
+const express = require('express');
+const db = require('./postDb');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	db.get()
-		.then((posts) => res.status(200).json(posts))
-		.catch(() =>
-			res.status(500).json({ errorMessage: "Unable to retrieve posts" })
-		);
-});
-
 router.use("/:id", validatePostId);
 
-router.get("/:id", (req, res) => {
-	db.getById(req.params.id)
-		.then((post) => res.status(200).json(post))
-		.catch(() =>
-			res.status(500).json({ errorMessage: "Unable to retrieve post" })
-		);
+router.get('/', (req, res) => {
+    db.get()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            res.status(500).json({
+                err,
+                errorMessage: "Can't retrieve posts."
+            });
+        });
 });
 
-router.delete("/:id", (req, res) => {
-	db.remove(req.params.id)
-		.then((count) => res.sendStatus(200))
-		.catch(() =>
-			res.status(500).json({ errorMessage: "Unable to remove post" })
-		);
+router.get('/:id', (req, res) => {
+    db.getById(req.params.id)
+        .then(post => {
+            res.status(200).json(post);
+        })
+        .catch(err => {
+            res.status(404).json({
+                err,
+                errorMessage: "Can't retrieve post."
+            });
+        });
 });
 
-router.put("/:id", (req, res) => {
-	db.update(req.params.id, req.body)
-		.then(() => {
-			db.getById(req.params.id)
-				.then((post) => res.status(201).json(post))
-				.catch(() =>
-					res.status(500).json({ errorMessage: "Unable to find updated post" })
-				);
-		})
-		.catch(() => {
-			res.status(500).json({ errorMessage: "Unable to update post" });
-		});
+router.delete('/:id', (req, res) => {
+    db.remove(req.params.id)
+        .then(count => {
+            count === 1
+                ? res.status(200).json({
+                      message: `${count} post was removed from database.`
+                  })
+                : res.status(500).json({
+                      err,
+                      errorMessage: "Can't remove post."
+                });
+        })
+        .catch(err => {
+            res.status(500).json({
+                err,
+                errorMessage: "Can't remove post."
+            });
+        });
+});
+
+router.put('/:id', (req, res) => {
+    db.update(req.params.id, req.body)
+        .then(count => {
+            count === 1
+                ? res.status(200).json({
+                      message: `${count} post was updated in database.`
+                  })
+                : res.status(500).json({
+                      err,
+                      errorMessage: "Can't update post."
+                  });
+        })
+        .catch(err => {
+            res.status(500).json({
+                err,
+                errorMessage: "Can't update post."
+            });
+        });
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-	db.getById(req.params.id)
-		.then((post) => {
-			if (post) {
-				next();
-			} else {
-				res.status(400).json({ message: "Invalid post id" });
-			}
-		})
-		.catch((err) =>
-			res
-				.status(500)
-				.json({ errorMessage: "Error connecting to posts database" })
-		);
+    db.getById(req.params.id)
+        .then(post => {
+            post
+                ? next()
+                : res.status(400).json({
+                    message: "Invalid post id." 
+                });
+        })
+        .catch(err => {
+            res.status(500).json({
+                err,
+                message: "Error connecting to database."
+            });
+        });
 }
 
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,112 +1,160 @@
-const express = require("express");
-
-const db = require("./userDb");
-const postDB = require("../posts/postDb");
+const express = require('express');
+const db = require('./userDb');
 
 const router = express.Router();
 
-router.post("/", validateUser, (req, res) => {
+router.post('/', validateUser, (req, res) => {
 	db.insert(req.body)
-		.then((user) => res.status(201).json(user))
-		.catch((err) =>
-			res.status(500).json({ errorMessage: "Unable to add user to database" })
-		);
-});
-
-router.get("/", (req, res) => {
-	db.get()
-		.then((users) => res.status(200).json(users))
-		.catch((err) =>
-			res.status(500).json({ errorMessage: "Unable to retrieve users" })
-		);
-});
-
-// id routes
-router.use("/:id", validateUserId);
-
-router.post("/:id/posts", validatePost, (req, res) => {
-	postDB
-		.insert({ ...req.body, ["user_id"]: req.params.id })
-		.then((post) => res.status(201).json(post))
-		.catch((err) =>
-			res.status(500).json({ errorMessage: "Unable to create post" })
-		);
-});
-
-router.get("/:id", (req, res) => {
-	db.getById(req.params.id)
-		.then((user) => res.status(200).json(user))
-		.catch((err) =>
-			res.status(500).json({ errorMessage: "Unable to retrieve user" })
-		);
-});
-
-router.get("/:id/posts", (req, res) => {
-	db.getUserPosts(req.params.id)
-		.then((posts) => res.status(200).json(posts))
-		.catch((err) =>
-			res.status(500).json({ errorMessage: "Unable to retrieve user posts" })
-		);
-});
-
-router.delete("/:id", (req, res) => {
-	db.remove(req.params.id)
-		.then((count) => res.sendStatus(200))
-		.catch((err) =>
-			res.status(500).json({ errorMessage: "Unable to remove user" })
-		);
-});
-
-router.put("/:id", validateUser, (req, res) => {
-	db.update(req.params.id, req.body)
-		.then((count) => {
-			db.getById(req.params.id)
-				.then((user) => res.status(200).json(user))
-				.catch((err) =>
-					res.status(500).json({ errorMessage: "Unable to find update user" })
-				);
+		.then(newUser => {
+			res.status(200).json(newUser);
 		})
-		.catch((err) =>
-			res.status(500).json({ errorMessage: "Unable to update user" })
-		);
+		.catch(err => {
+			res.status(500).json({
+				err,
+				errorMessage: "Can't make new user."
+			});
+		});
+});
+
+router.post('/:id/posts', validatePost, (req, res) => {
+	db.insert(req.body)
+		.then(newPost => {
+			res.status(200).json(newPost);
+		})
+		.catch(err => {
+			res.status(500).json({
+				err,
+				errorMessage: "Can't make new post."
+			});
+		});
+});
+
+router.get('/', (req, res) => {
+	db.get()
+		.then(users => {
+			res.status(200).json(users);
+		})
+		.catch(err => {
+			res.status(500).json({
+				err,
+				errorMessage: "Can't retrieve users."
+			});
+		});
+});
+
+router.get('/:id', validateUserId, (req, res) => {
+	db.getById(req.params.id)
+		.then(user => {
+			res.status(200).json(user);
+		})
+		.catch(err => {
+			res.status(404).json({
+				err,
+				errorMessage: "Can't retrieve user."
+			});
+		});
+});
+
+router.get('/:id/posts', validateUserId, (req, res) => {
+	db.getUserPosts(req.params.id) 
+		.then(posts => {
+			res.status(200).json(posts);
+		})
+		.catch(err => {
+			res.status(500).json({
+				err,
+				errorMessage: "Can't retrieve users posts."
+			});
+		});
+});
+
+router.delete('/:id', validateUserId, (req, res) => {
+	db.remove(req.params.id)
+		.then(count => {
+			count === 1
+				? res.status(200).json({
+					message: `${count} user was removed from database.`
+				})
+				: res.status(500).json({
+					err,
+					errorMessage: "Can't remove user."
+				});
+		})
+		.catch(err => {
+			res.status(500).json({
+				err,
+				errorMessage: "Can't remove user."
+			});
+		})
+});
+
+router.put('/:id', validateUser, (req, res) => {
+	db.update(req.params.id, req.body)
+		.then(count => {
+			count === 1
+				? res.status(200).json({
+					message: `${count} user was updated in database.`
+				})
+				: res.status(500).json({
+					err,
+					errorMessage: "Can't update user."
+				});
+		})
+		.catch(err => {
+			res.status(500).json({
+				err,
+				errorMessage: "Can't update user."
+			});
+		})
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
 	db.getById(req.params.id)
-		.then((user) => {
+		.then(user => {
 			if (user) {
 				req.user = user;
 				next();
 			} else {
-				res.status(400).json({ message: "Invalid user id" });
+				res.status(400).json({
+					message: "Invalid user id." 
+				});
 			}
 		})
-		.catch((err) =>
-			res
-				.status(500)
-				.json({ errorMessage: "Error connecting to users database" })
-		);
-}
+		.catch(err => {
+			res.status(500).json({
+				err,
+				message: "Error connecting to database."
+			});
+		});
+};
 
 function validateUser(req, res, next) {
-	if (Object.keys(req.body).length !== 0) {
-		req.body.name
+	if(Object.keys(req.body).length !== 0) {
+		req.body.name 
 			? next()
-			: res.status(400).json({ message: "Missing required name field" });
+			: res.status(400).json({
+				message: "missing required name field"
+			});
 	} else {
-		res.status(400).json({ message: "Missing user data" });
+		res.status(400).json({
+			errorMessage: "missing user data"
+		});
 	}
 }
 
 function validatePost(req, res, next) {
-	if (Object.keys(req.body).length !== 0) {
-		req.body.text
-			? next()
-			: res.status(400).json({ message: "Missing required text field" });
+	if(Object.keys(req.body).length !== 0) {
+		req.body.text 
+			? next ()
+			: res.status(400).json({
+				errorMessage: "missing required text field."
+			})
 	} else {
-		res.status(400).json({ message: "Missing post data" });
+		res.status(400).json({
+			errorMessage: "missing post data"
+		});
 	}
 }
 
