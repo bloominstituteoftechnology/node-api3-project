90c7c9232abb592e3b10bdd3552458c97d6ef7dd
Your name <a.m.verdine@gmail.com>
2020-04-17 01:04:56 -0400

    Write GET request to retrive Users

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
 server.use(express.json());
-server.use(cors);
+server.use(cors());
+server.use(logger);
+
+server.use("/users", userRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -13,17 +17,9 @@ server.get("/", (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-  switch (options.format) {
-    case "short":
-      console.log(`${req.method} ${req.url}`);
-      break;
-    case "long":
-    default:
-      console.log(
-        `${new Date().toISOString()} ${req.ip} ${req.method} ${req.url} `
-      );
-      break;
-  }
+  console.log(
+    `${new Date().toISOString()} ${req.ip} ${req.method} ${req.url} `
+  );
 
   next();
 }
