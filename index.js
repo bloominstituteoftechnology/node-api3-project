a2565042d9a8d4b2056af9b8c417e8a79e749bb5
Your name <a.m.verdine@gmail.com>
2020-04-19 22:30:53 -0400

    More editing

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ const server = require("./server");
 
 const port = process.env.PORT || 4000;
 
-server.get("/", (req, res) => {
-  res.status(200).json({
-    message: `Welcome ${process.env.COHORT}`,
-    fact: process.env.FUN_FACT || "I have no fun facts",
-  });
-});
-
 server.listen(port, () => {
   console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
