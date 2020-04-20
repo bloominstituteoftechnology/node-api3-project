51865b3fb76fa99d8095b8bfcc0d2e91da1380a3
Your name <a.m.verdine@gmail.com>
2020-04-20 14:58:44 -0400

    Implement .env changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // code away!
+require("dotenv").config();
 const server = require("./server");
 
 const port = process.env.PORT || 4000;
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const userRouter = require("./users/userRouter");
