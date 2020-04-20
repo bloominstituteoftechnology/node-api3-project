9cd67949a6f698879eaf8254f810a7f2783470f8
Your name <a.m.verdine@gmail.com>
2020-04-20 15:18:22 -0400

    Port changes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const userRouter = require("./users/userRouter");
 const postRouter = require("./posts/postRouter");
 
 const server = express();
+const port = process.env.PORT || 4000;
 
 server.use(express.json());
 server.use(cors());
