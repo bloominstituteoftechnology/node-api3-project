7e58d41f108bdf12776f3239c10fbce7ab259083
Your name <a.m.verdine@gmail.com>
2020-04-19 22:34:27 -0400

    Moved require().config()

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // code away!
-require("dotenv").config();
 const server = require("./server");
 
 const port = process.env.PORT || 4000;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const userRouter = require("./users/userRouter");
