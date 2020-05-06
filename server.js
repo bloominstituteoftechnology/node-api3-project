377ac7adbbb337cc6b14b14d0d1b5e00495cdb93
andrewhpark25 <andrewhpark25@gmail.com>
2020-05-06 16:35:56 -0700

    initial setup

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
+const usersRouter = require("./users/userRouter.js");
+
 const server = express();
+
+server.use(express.json()); 
+
+server.use("/api/users", logger, usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +14,12 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+
+  const today = new Date().toISOString(); // YYYY-MM-DD
+    console.log(`[${today}] ${req.method} to ${req.url}`);
+  
+    next();
+}
 
 module.exports = server;
