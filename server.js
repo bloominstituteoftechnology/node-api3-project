21b239cbcad1af55b96e2a7e106bf943c22da1ae
robertelias <robert.elias@live.com>
2020-05-06 16:19:13 -0700

    updates to server.js

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
@@ -6,8 +10,21 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+// global middleware
+server.use(express.json()); // built in middleware, no need to npm install it
+server.use(morgan("combined")); // third party
+server.use(helmet());
+
+
+//routes end poitns
+server.use("/api/users", userRouter);
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+     const today = new Date().toISOString(); // YYYY-MM-DD
+   console.log(`[${today}] ${req.method} to ${req.url}`
+   );
+   next();
+};
 
 module.exports = server;
