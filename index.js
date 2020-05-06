769fcd45761d7e976dc9062c4461c13789cc30d3
Golden Eagle <joowoon.kang@biola.edu>
2020-05-06 13:55:23 -0700

    logger added

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require("./server.js");
+
+server.listen(4000, () => {
+  console.log("\n* Server Running on http://localhost:4000 *\n");
+});
--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express = require('express');
+const express = require("express");
 
+const userDb = require("./users/userDb");
+const postDb = require("./posts/postDb");
+const userRouter = require("./users/userRouter");
+const postRouter = require("./posts/postRouter");
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json());
+
+server.use(logger);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const today = new Date().toISOString();
+  console.log(`[${today}]${req.method} to ${req.url}`);
+  next();
+}
 
 module.exports = server;
