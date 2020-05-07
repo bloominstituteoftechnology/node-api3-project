1135ae50fcae73a9bfea9dec3428ce6e7351d2bc
andrewhpark25 <andrewhpark25@gmail.com>
2020-05-07 10:22:05 -0700

    deploy

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
 const server = require('./server.js');
 
-server.listen(4000, () => {
-  console.log('\n* Server Running on http://localhost:4000 *\n');
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost:${port}  *\n`);
 });
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+
 //custom middleware
 
 function logger(req, res, next) {
