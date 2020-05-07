05b536e1945d12bb6d5128d4061f8c6f346deaa6
Michael Bidnyk <mbidnyk1@gmail.com>
2020-05-06 21:39:01 -0500

    added custom middleware

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require('./server.js');
+
+server.listen(5002, () => {
+  console.log('\n* Server Running on http://localhost:5002 *\n');
+});
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 
 const server = express();
-
-server.get('/', (req, res) => {
+server.use(express.json())
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+
+function logger(req, res, next) {
+  const today = new Date().toTimeString()
+  console.log(`${today} ${req.method} to ${req.url}`)
+  next()
+}
+
+
 
 module.exports = server;
