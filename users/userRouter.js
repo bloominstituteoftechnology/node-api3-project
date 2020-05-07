1f16870fae2709a16462ad7ffac58c240cab7d98
robertelias <robert.elias@live.com>
2020-05-06 17:20:17 -0700

    updated post validateUser

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
+const Users = require("./userDb.js");
 const router = express.Router();
 
-router.post('/', (req, res) => {
+
+
+router.post('/', validateUser, (req, res) => {
   // do your magic!
+  Users.insert(req.body)
+    .then(newUser => {
+    res.status(201).json(newUser)
+  })
+  .cath(error => {
+    console.log(error)
+    res.status(500).json({message: "there was an issue adding user."})
+  })
 });
 
 router.post('/:id/posts', (req, res) => {
