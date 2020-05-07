40846fce33658284002579592db11f43429f0a29
robertelias <robert.elias@live.com>
2020-05-06 18:01:49 -0700

    validateUser middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -71,7 +71,14 @@ function validateUserId (req, res, next) {
 
 function validateUser (req, res, next) {
   // do your magic!
-}
+  if(!req.body){
+    res.status(400).json({message: "missing user data"})
+  }else if(!req.body.name){
+    res.status(400).json({message : "missing required name feild"})
+  }else{
+    next();
+  }
+  }
 
 function validatePost (req, res, next) {
   // do your magic!
