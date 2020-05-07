a361909c8fb5967618d56530fba2b618c7146ff9
Gordon Caister <gordoncaister@gmail.com>
2020-05-06 20:15:35 -0500

    mvp

--- server.js
+++ server.js
@@ -2,12 +2,37 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json())
+
+const postRouter = require('./posts/postRouter.js');
+
+const userRouter = require('./users/userRouter');
+
+server.use(express.json());
+
+server.use('/api/posts', logger, postRouter);
+server.use('/api/users', logger, userRouter);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
+
+  next();
+}
+
+server.use(logger);
+
+
 
 module.exports = server;
+
+
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,126 @@
 const express = require('express');
 
 const router = express.Router();
+const Users = require("./userDb")
+const Posts = require("../posts/postDb")
 
-router.post('/', (req, res) => {
+router.post('/',validateUser, (req, res) => {
   // do your magic!
+  Users.insert({name:req.body.name})
+    .then(result => {
+      res.status(201).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({error: "Error while creating a new user"})
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts',validateUserId,validatePost, (req, res) => {
   // do your magic!
+  Posts.insert({...req.body,user_id:req.user.id})
+    .then(post=>{
+      console.log(post)
+      res.status(201).json(post)
+    })
+  
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  Users.get()
+    .then((users)=>{
+        res.status(201).json(users)
+    })
+    .catch(()=>{
+      res.status(500).json({error:"Could not get list of users"})
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id',validateUserId, (req, res) => {
   // do your magic!
+  Users.getById(req.params.id)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(()=>{
+      res.status(500).json({error:"Error finding user data"})
+    })
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts',validateUserId, (req, res) => {
   // do your magic!
+  Users.getUserPosts(req.params.id)
+  .then(posts => {
+    res.status(201).json(posts)
+  })
+  .catch(()=>{
+    res.status(500).json({error:"Error finding users post data"})
+  })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',validateUserId, (req, res) => {
   // do your magic!
+  Users.remove(req.params.id)
+    .then(()=> {
+      res.status(200).json({message:"User Deleted"})
+    })
+
+  .catch(()=>{
+      res.status(500).json({error:"Error Deleting User"})
+    })
+  
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',validateUserId,validateUser, (req, res) => {
   // do your magic!
+  Users.update(req.params.id,req.body)
+    .then(()=> {
+      res.status(200).json({message:"User updated"})
+    })
+
+  .catch(()=>{
+      res.status(500).json({error:"Error Updating User"})
+    })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
+
   // do your magic!
+  Users.getById(req.params.id)
+    .then((response)=>{
+      if(response){
+        req.user = response;
+        next();
+      } else {
+        res.status(400).json({message: "invalid user id"})
+      }
+    })
+    .catch((err)=>{
+      res.status(500).json({error: "There was a problem finding the user ID"})
+    })
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  if (!req.body){
+    res.status(400).json({ message: "missing post data" })
+  } else if (!req.body.name) {
+    res.status(400).json({ message: "missing required text field" })
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if(!req.body){
+    res.status(400).json({message: "missing post data"})
+  } else if (!req.body.text){
+    res.status(400).json({message: "missing required text field"})
+  } else {
+    next();
+  }
 }
 
 module.exports = router;
