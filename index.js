c9591c6e9f8feddc146d00ed556e24efe2692244
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-05-07 14:45:25 -0700

    Added/changed a few more things to push up to github so the app will hopefully deploy

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // code away!
+require("dotenv").config();
 const server = require ('./server')
 
 const port = process.env.PORT || 5000;
-
 server.listen(port, () => {
-  console.log('\n* Server is running on http://localhost: ${port} *\n')
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+
