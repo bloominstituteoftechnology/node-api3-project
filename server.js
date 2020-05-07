b5e17e552e5fa09c5ce9b4421f5a1fc905a3a2be
robertelias <robert.elias@live.com>
2020-05-07 13:11:11 -0700

    updates

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ server.use(cors());
 server.use('/api/users', userRouter);
 server.use('/api/posts', postRouter);
 
-server.get('/', (req, res) => {
+server.get('/api', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
