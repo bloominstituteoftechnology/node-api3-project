9dbe0ecbdc7cd23c9f40b86f33f7173a51aad4df
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-05-07 15:10:59 -0700

    Made more changes to get Heroku running

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // code away!
 require("dotenv").config();
-const server = require ('./server')
+
+const server = require ('./server');
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
