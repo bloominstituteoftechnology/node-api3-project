4d33daac44fd9e8d42dec3fb9053680cc237bfbd
Patrick Dal Gobbo <juandalgobbo@Juans-Air.lan1>
2020-05-07 18:02:25 -0400

    deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // code away!
 const server = require("./server");
 
-server.listen(8000, () => {
-    console.log("Listening on port 8000")
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
