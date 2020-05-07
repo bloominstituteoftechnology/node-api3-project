b49e3a0811065c1757dfd69db64489fb0d42404d
robertelias <robert.elias@live.com>
2020-05-06 17:55:04 -0700

    Get user by id

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,73 +1,79 @@
-const express = require('express');
-const Users = require("./userDb.js");
-const Posts = require('../posts/postDb');
-const router = express.Router();
+const express = require ('express');
+const Users = require ('./userDb.js');
+const Posts = require ('../posts/postDb');
+const router = express.Router ();
 
-
-
-router.post('/', validateUser, (req, res) => {
+router.post ('/', validateUser, (req, res) => {
   // do your magic!
-  Users.insert(req.body)
-    .then(newUser => {
-    res.status(201).json(newUser)
-  })
-  .cath(error => {
-    console.log(error)
-    res.status(500).json({message: "there was an issue adding user."})
-  })
-});
-
-router.post('/:id/posts', validatePost, validateUserId, (req, res) => {
-  // do your magic!
-  Posts.insert({...req.body, user_id: req.params.id})
-    .then(newPost => {
-      res.status(201).json(newPost)
+  Users.insert (req.body)
+    .then (newUser => {
+      res.status (201).json (newUser);
     })
-    .cath(error => {
-      console.log(error)
-      res.status(500).json({message: "there was an issue adding posts."})
+    .cath (error => {
+      console.log (error);
+      res.status (500).json ({message: 'there was an issue adding user.'});
+    });
+});
+
+router.post ('/:id/posts', validatePost, validateUserId, (req, res) => {
+  // do your magic!
+  Posts.insert ({...req.body, user_id: req.params.id})
+    .then (newPost => {
+      res.status (201).json (newPost);
     })
-  });
-
-router.get('/', (req, res) => {
-  // do your magic!
-  Users.get()
-    .then(users => {
-    res.status(200).json(users)
-  })
-  .catch(error => {
-    console.log(error)
-    res.status(500).json({message: "failed to get users."})
-  })
+    .cath (error => {
+      console.log (error);
+      res.status (500).json ({message: 'there was an issue adding posts.'});
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get ('/', (req, res) => {
+  // do your magic!
+  Users.get ()
+    .then (users => {
+      res.status (200).json (users);
+    })
+    .catch (error => {
+      console.log (error);
+      res.status (500).json ({message: 'failed to get users.'});
+    });
+});
+
+router.get ('/:id', (req, res) => {
+  // do your magic!
+  Users.getById (req.params.id)
+    .then (users => {
+      res.status (200).json (users);
+    })
+    .catch (error => {
+      console.log (error);
+      res.status (500).json ({message: 'failed to get users by id.'});
+    });
+});
+
+router.get ('/:id/posts', (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
+router.delete ('/:id', (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put ('/:id', (req, res) => {
   // do your magic!
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId (req, res, next) {
   // do your magic!
 }
 
-function validateUser(req, res, next) {
+function validateUser (req, res, next) {
   // do your magic!
 }
 
-function validatePost(req, res, next) {
+function validatePost (req, res, next) {
   // do your magic!
 }
 
