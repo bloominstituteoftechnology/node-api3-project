441b1be1877c2ef115d19ec6afa78623647e6037
Patrick Dal Gobbo <juandalgobbo@Juans-Air.lan1>
2020-05-06 23:23:13 -0400

    created validateUserId and passed it into its respective requests

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
+const User = require('./userDb');
+const postDb = require('../posts/postDb');
+
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/',validateUser, (req, res) => {
   // do your magic!
+  db.get()
+  .then()
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -12,28 +17,93 @@ router.post('/:id/posts', (req, res) => {
 
 router.get('/', (req, res) => {
   // do your magic!
+  User.get()
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({error:"sorry couldnt get users"})
+  })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
+  // const { id } = req.params;
+  res.status(200).json(req.user)
+  // User.getById(id)
+  // .then(user => {
+  //   if (user) {
+  //     res.status(200).json(user)
+  //   }else {
+  //     res.status(404).json({error: "Sorry, user with that ID does not exist"})
+  //   }
+  // })
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
+  const { id } = req.params;
+  User.getUserPosts(id)
+  .then(posts => res.status(200).json(posts))
+  .catch(err => {
+    console.log(err);
+    res.status(404).json({error:"Error getting user posts"})
+  })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
+  const { id } = req.user;
+  User.remove(id)
+    .then(() => res.status(204).end())
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({error:"Sorry error deleting user"})
+    })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
   // do your magic!
+  const { id } = req.params;
+  const { name } = req.body;
+  User.update(id, {name})
+        .then(updated => {
+          if (updated) {
+            User.getById(id)
+            .then(user => res.status(200).json(user))
+            .catch(err => {
+              console.log(err);
+              res.status(500).json({error: "Error getting user"})
+            })
+          }
+          
+       })
+       .catch( err => {
+         console.log(err);
+         res.status(500).json({error: "Error updating user"})
+       });
 });
+    
+     
 
 //custom middleware
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const { id } = req.params;
+  User.getById(id)
+    .then((user) => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(404).json({message: "Sorry invalid user ID"});
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({errorMessage:"Error connecting"})
+    })
 }
 
 function validateUser(req, res, next) {
