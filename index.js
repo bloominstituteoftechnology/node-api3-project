5b9b6e91ee14d03871ab3d68cdf9f79ef68cc0b7
DenisChuvilin <denischuv@gmail.com>
2020-06-04 16:03:35 -0700

    added port

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ server.get('/', (req, res) => {
   res.status(200).json({ environment: process.env.NODE_ENV, port: process.env.PORT });
 });
 
+const port = process.env.PORT || 5000;
 server.listen(5000, () => {
   console.log(
     '------------------------------------------\nserver is running on http://localhost:5000\n------------------------------------------ '
