9708e1d22dbcafd271c5540fdbae57d8c0f0ec9a
Your Name <Reginaldbmiller@gmail.com>
2020-06-08 21:15:22 -0500

    logger function complete

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,13 +1,7 @@
 module.exports = (format) => {
     return (req, res, next) => {
-        switch (format){
-            case "short":
-                console.log(`${Date.now()} ${req.method} ${req.url}`)
-                break
-            case "long":
-                default:
                 const time = new Date().toISOString()
-                console.log(`[${time}] ${req.ip} ${req.method} ${req.url}`)
+                console.log(`[${time}], ${req.method}, ${req.url}`)
 
                 next()
-}}};
+}};
---
+++ middleware/validatePost.js
@@ -0,0 +1,7 @@
+module.exports = () => {
+    return (req, res, next) => {
+
+
+        
+    }
+};
