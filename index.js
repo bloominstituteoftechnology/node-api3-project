40f97cae93589d770304bd97c87645bec93d1bb8
remoo1901 <remoo1901@hotmail.com>
2020-06-09 20:05:14 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code away!
+const express = require("express")
+const morgan = require("morgan")
+const welcomeRouter = require("./welcome/welcomeRouter")
+const userRouter = require("./users/userRouter")
+const postRouter = require("./posts/postDb")
+
+const logger = require("./middleware/logger")
+
+
+const server = express();
+const port = 5000;
+
+server.use(express())
+
+server.use(logger())
+
+server.use(welcomeRouter);
+server.use(userRouter);
+server.use(postRouter);
+
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+        message: " SOMETHING WENT WRONG",
+    })
+})
+
+
+
+server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`)
+})
