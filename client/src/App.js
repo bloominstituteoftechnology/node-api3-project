a436251c760e771ac1c4d2157c722815260049b3
Andrew Haberman <a.haberman25@gmail.com>
2020-06-10 13:29:33 -0400

    simple cra

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    axios
+      .get('http://localhost:4000/api/users')
+      .then(res => {
+        console.log(res)
+        setUsers(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>test app</h1>
+        {users.map((user) => (
+          <p key={user.id}>{user.name}</p>
+        ))}
+      </header>
+    </div>
+  );
+}
+
+export default App;
