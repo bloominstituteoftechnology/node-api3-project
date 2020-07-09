a2e8d68efcac1833441439b731b6c25b266bdec0
jasonelliots <j.elliotschwartz@gmail.com>
2020-07-09 12:18:41 -0700

    prepared for deployment

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // code away!
 
+
 const server = require("./server.js");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`\n* Server Running on http://localhost:${PORT} *\n`);
 });
