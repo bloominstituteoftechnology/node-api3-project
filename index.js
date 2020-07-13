683e375e2cff772f2d28c70d1a61a5e41908fae8
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-07-13 11:20:19 -0700

    env support added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const server = require("./server.js");
-const PORT = 8000;
-server.listen(PORT, () => {
-  console.log(`\n* Server Running on http://localhost:${PORT} *\n`);
+//const PORT = 8000;
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
