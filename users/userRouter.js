f0d89564bfac0debce1f974fa58761fe53e0d017
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-07-14 16:39:40 -0700

    completed CRUD operations

--- users/userRouter.js
+++ users/userRouter.js
@@ -21,6 +21,14 @@ router.post('/',validateUser, (req, res) => {
 
 router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   console.log('req.body', req.body)
+  const postInfo = {...req.body, user_id: req.params.id};
+  Posts.insert(postInfo)
+  .then(post =>{
+    res.status(210).json(post);
+  })
+  .catch(error =>{
+    res.status(500).json({message: 'error adding the post'})
+  })
 });
 
 router.get('/', (req, res) => {
@@ -51,11 +59,33 @@ router.get('/:id/posts', validateUserId, (req, res) => {
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
-  // do your magic!
+  Helpers.remove(req.params.id)
+  .then(count =>{
+    if(count >0) {
+      res.status(200).json({message: "the user has been removed"});
+    } else {
+      res.status(404).json({ message: 'the user could not be found'});
+    }
+  })
+  .catch(error=>{
+    console.log(error);
+    res.status(500).json({message: 'Error removing the user'})
+  });
 });
 
-router.put('/:id', validateUserId, (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId, validateUser, (req, res) => {
+  Helpers.update(req.params.id, req.body)
+  .then(user=>{
+    if(user){
+    res.status(200).json(user);
+  }else{
+    res.status(404).json({message: "the user could not be found"});
+  }
+  })
+  .catch(error =>{
+    console.log(error)
+    res.status(500).json({ message: 'Error updating the user'});
+  })
 });
 
 //custom middleware
