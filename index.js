3e913fbf260f8b244f7f1d29ac129d81eb7c6517
John Rossi <DACOOK1983@gamil.com>
2020-07-14 22:06:57 -0400

    added .env, added script node start

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // code away!
+require('dotenv').config();
 const server = require('./server.js')
 
-server.listen(5000, () => {
-  console.log('\n* Server Running on http://localhost:5000 *\n');
+const port = process.env.PORT
+
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
