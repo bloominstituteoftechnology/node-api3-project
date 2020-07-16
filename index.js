36388fa0b388fb57ea0093b26d48b70c2aa1289a
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-07-16 13:00:22 -0700

    dynamic port

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const server = require('./server.js');
 
-server.listen(4000, () => {
-    console.log('\n* Server running on http://localhost:4000 *\n');
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`\n* Server running on http://localhost:${port} *\n`);
 });
