12241354321a4c42483adb0794e68fe33e2131d2
Brian Clayton <finalb@projectfinalboss.com>
2020-07-16 19:39:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 
 const server = express();
+
+server.use(express.json());
+server.use('/api/posts', postRouter);
+server.use('api/user', userRouter);
+server.get(helmet());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +16,9 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} request`);
+  next();
+}
 
 module.exports = server;
