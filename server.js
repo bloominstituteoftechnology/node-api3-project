1a94e9356c973545301d8b2ce2daa497dcdde35e
JazmineMT <jazmineamcginnis@gmail.com>
2020-08-05 11:28:57 -0700

    MW logger done

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const server = express();
 
+server.use(logger)
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString}] a ${req.method} to ${req.url}`);
+  next();
+}
 
 module.exports = server;
