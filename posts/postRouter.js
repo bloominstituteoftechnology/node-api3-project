9d2b8716438c406238e9bd82debf90f697da55e9
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-05 18:00:49 -0700

    DELETE post by id

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -32,7 +32,20 @@ router.get("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
-  // do your magic!
+  const postID = req.params.id;
+
+  postDB
+    .remove(postID)
+    .then((post) => {
+      if (post) {
+        res.status(204).end();
+      } else {
+        res.status(400).json({ message: "Invalid post id." });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 });
 
 router.put("/:id", (req, res) => {
