a4b084f6afc1683a04b22a5074c58caa727502c1
Alex Kemper <alexkemper@MacBook-Pro.local>
2020-08-06 16:53:05 -0400

    trying again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config;
 
 const server = require("./server");
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(`*** server running on http://localhost:${port}`);
