eaab769229be3d38a4154fc33852c6dc157c0021
Alex Kemper <alexkemper@MacBook-Pro.local>
2020-08-06 18:11:05 -0400

    reinstalled dependencies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config;
 
 const server = require("./server");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
   console.log(`*** server running on http://localhost:${port}`);
