81e050594562d7e33c88c4baa577306f44c13c58
Ben Venker <bvemails@gmai.com>
2020-08-11 11:12:50 -0500

    add middleware, duh

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,61 +1,69 @@
-const express = require("express");
-const users = require("../users/userDb");
-const posts = require("../posts/postDb");
-const validatePost = require("../middleware/validatePost");
-const validateUser = require("../middleware/validateUser");
+const express = require('express');
+const users = require('../users/userDb');
+const posts = require('../posts/postDb');
+const logger = require('../middleware/logger');
+const validatePost = require('../middleware/validatePost');
+const validateUser = require('../middleware/validateUser');
+const validateUserId = require('../middleware/validateUserId');
 const router = express.Router();
 
-router.post("/", validateUser(), (req, res) => {
+router.post('/', validateUser(), (req, res) => {
   // users
   //   .insert(req.body.name)
   //   .then((res) => res.status(201).json({ message: "User created" }))
   //   .catch((err) => res.status(500).json({ message: err }));
   users
     .insert(req.body)
-    .then((user) => res.status(200).json(user))
-    .catch((err) => res.status(500).json({ message: err }));
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({ message: err }));
 });
 
-router.post("/:id/posts", validatePost(), (req, res) => {
-  posts
-    .insert({ text: req.body.text, user_id: req.params.id })
-    .then((post) => res.status(201).json({ post }))
-    .catch((err) => res.status(500).json({ message: err }));
-});
+router.post(
+  '/:id/posts',
+  logger(),
+  validateUserId(),
+  validatePost(),
+  (req, res) => {
+    posts
+      .insert({ text: req.body.text, user_id: req.params.id })
+      .then(post => res.status(201).json({ post }))
+      .catch(err => res.status(500).json({ message: err }));
+  }
+);
 
-router.get("/", (req, res) => {
+router.get('/', logger(), (req, res) => {
   users
     .get()
-    .then((users) => res.status(200).json(users))
-    .catch((err) => status(500).json({ message: err }));
+    .then(users => res.status(200).json(users))
+    .catch(err => status(500).json({ message: err }));
 });
 
-router.get("/:id", (req, res) => {
+router.get('/:id', logger(), validateUserId(), (req, res) => {
   users
     .getById(req.params.id)
-    .then((user) => res.status(200).json(user))
-    .catch((err) => res.status(500).json({ message: err }));
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({ message: err }));
 });
 
-router.get("/:id/posts", (req, res) => {
+router.get('/:id/posts', logger(), validateUserId(), (req, res) => {
   users
     .getUserPosts(req.params.id)
-    .then((posts) => res.status(200).json(posts))
-    .catch((err) => res.status(500).jsonn({ message: err }));
+    .then(posts => res.status(200).json(posts))
+    .catch(err => res.status(500).jsonn({ message: err }));
 });
 
-router.delete("/:id", (req, res) => {
+router.delete('/:id', logger(), validateUserId(), (req, res) => {
   users
     .remove(req.params.id)
-    .then((user) => res.status(200).json(user))
-    .catch((err) => res.status(500).json({ message: err }));
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({ message: err }));
 });
 
-router.put("/:id", (req, res) => {
+router.put('/:id', logger(), validateUserId(), (req, res) => {
   users
     .update(req.params.id, req.body)
-    .then((user) => res.status(200).json(user))
-    .catch((err) => res.status(500).json({ message: err }));
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({ message: err }));
 });
 
 //custom middleware
