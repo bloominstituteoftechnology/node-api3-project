cb5a5cf1a1cf2045a410f5dffcb97e82461f7681
Ryan Schueck <ryschueck@gmail.com>
2020-08-10 21:33:58 -0400

    finished all the end points and middleware for MVP

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const usersRouter = require('./users/userRouter.js');
 
 const server = express();
+
+server.use(logger);
+server.use(express.json());
+server.use(usersRouter);
+
+server.use((err, req, res, next) => {
+	// log the error and send back generic message so we don't expose
+	// any potentailly-sensitive information contained in the error message
+	console.log(err)
+	
+	res.status(500).json({
+		message: "Something went wrong, please try again later",
+	})
+})
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +23,16 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+// LOGGER MIDDLEWARE
+function logger(req, res, next) {
+  const method = req.method;
+  const endpoint = req.originalUrl;
+  let date = new Date();
+
+  console.log(`${method} to ${endpoint} at ${date}`);
+  next();
+}
+
+
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,127 @@
 const express = require('express');
+const userDb = require('./userDb');
+const postDb = require('../posts/postDb')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // do your magic!
+  userDb.insert(req.body)
+  .then(user => {
+    res.status(201).json(user)
+  })
+  .catch((error)=>{
+    next(error)
+  })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts',validateUserId, validatePost, (req, res) => {
   // do your magic!
+  req.body.user_id = req.params.id;
+  postDb.insert(req.body)
+  .then((post)=> {
+    res.status(201).json(post);
+  })
+  .catch((error)=> {
+    next(error)
+  })
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  userDb.get(req.query)
+  .then((users)=>{
+    res.status(200).json(users)
+  })
+  .catch((error)=> {
+    next(error)
+  })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
+  userDb.getById(req.params.id)
+  .then((user)=>{
+    if(user){
+      res.status(200).json(user)
+    }
+  })
+  .catch((error)=>{
+    next(error)
+  })
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
+  userDb.getUserPosts(req.params.id)
+  .then((posts)=> {
+    res.status(200).json(posts)
+  })
+  .catch((error)=> {
+    next(error)
+  })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',validateUserId, (req, res) => {
   // do your magic!
+  userDb.remove(req.params.id)
+  .then((user)=> {
+    res.status(200).json({message: "User has been terminated"})
+  })
+  .catch((error)=> {
+    next(error)
+  })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',validateUserId, (req, res) => {
   // do your magic!
+  userDb.update(req.params.id, req.body)
+  .then((user)=> {
+    res.status(201).json(user)
+  })
+  .catch((error)=>{
+    next(error)
+  })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
   // do your magic!
+  userDb.getById(req.params.id)
+    .then((user)=>{
+      if(user){
+        req.user = user;
+        next();
+      }else{
+        res.status(400).json({message: "invalid user id"})
+      }
+    })
+    .catch((error)=>{
+      console.log(error)
+      res.status(500).json({error: 'There was a problem pulling the data from the server'})
+    })
+  
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+    if(!req.body){
+      return res.status(400).json({message: "missing user data" })
+    }else if(!req.body.name){
+      return res.status(400).json({message: "missing required name field"})
+    }
+    next();
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  if(!req.body){
+    return res.status(400).json({message: "missing post data"})
+  }else if(!req.body.text){
+    return res.status(400).json({message: "missing required text field"})
+  }
+  next()
 }
 
 module.exports = router;
