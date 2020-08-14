abc427bca035c3aecd134cdb4cacbb8ae4db9a0f
Valerie Vazquez <vnvazquez68@gmail.com>
2020-08-13 19:52:27 -0600

    added dotenv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./server.js");
-
-const PORT = 8000;
+require("dotenv").config();
+const port = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
   console.log(`** Server Running on http://localhost:${PORT} **`);
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 8000;
 server.use(logger());
 server.use(express.json());
 server.get("/", (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2>Welcome to Hobbiton!</h2>`);
 });
 
 //custom middleware
