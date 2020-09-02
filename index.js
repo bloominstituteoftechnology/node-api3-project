ea683340b5c5d5e98fee7eb9e29d8c17b12357d9
Ashley <ashley.bergsma42@gmail.com>
2020-09-02 13:59:43 -0700

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code away!
+const port = 9000; 
+const server = require('./server'); 
+
+server.listen(port, () => console.log(`server is listenting on ${port}`)); 
--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
+const userRouter = require('./users/userRouter'); 
+
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use('/api/posts', userRouter); 
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`A ${req.method} request was made to ${req.url} at ${req.timestamp} on ${req.date}`);
+  next(); 
+}
 
 module.exports = server;
