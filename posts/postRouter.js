7edfcda5fe71e23b8919b29ab0e3fff51efbad1c
spazdrum <spazdrum@gmail.com>
2020-09-08 19:37:09 -0400

    finished postRouter

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,71 @@
-const express = require('express');
+const express = require("express");
+const data = require("./postDb");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get("/", (req, res) => {
+  data
+    .get()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "there is a server issue" });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  data
+    .getById(id)
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch(() => {
+      res.status(500).json({ error: `there is no id matching ${id}` });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  data
+    .remove(id)
+    .then((post) => {
+      res.status(204).json(post);
+    })
+    .catch(() => {
+      res.status(404).json({ error: `there is no id matching ${id}` });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const change = req.body;
+  data
+    .update(id, change)
+    .then((one) => {
+      if (one === 1) {
+        res.status(200).json(one);
+      } else {
+        res.status(404).json({ message: "please provide the correct id" });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Server error" });
+    });
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  data.getById(req.params.id).then((post) => {
+    if (post) {
+      console.log("post", post);
+      next();
+    } else {
+      res.status(400).json({ message: error.message });
+    }
+  });
 }
 
 module.exports = router;
