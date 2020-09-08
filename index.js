4f42eb56370fcafd074f64ec4d21ea1555a3ff73
spazdrum <spazdrum@gmail.com>
2020-09-08 19:21:20 -0400

    finished index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code away!
+const express = require("express");
+const postRouter = require("./posts/postRouter.js");
+const userRouter = require("./users/userRouter.js");
+
+const server = express();
+
+server.use(express.json());
+server.use("/api/posts", postRouter);
+server.use("/api/users", userRouter);
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`running on port : ${port}`);
+});
