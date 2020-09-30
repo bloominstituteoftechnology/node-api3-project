24602982168b318ff7d81f73a8dad9488e198d27
mmitch2958 <mikescottmitchell@outlook.com>
2020-09-30 17:13:57 -0400

    finished router endpoints

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code away!
+onst express = require('express');
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
+
+server.get('/', (req, res) => {
+    res.send('server is running');
+})
+
+server.listen(5000, () => console.log("API is running on port 5000"));
+
+module.exports = server
