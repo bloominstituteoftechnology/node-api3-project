d9eec420af26303ce999708ac6d3c0550856d35b
ajg7 <ajgebara@gmail.com>
2020-09-30 15:57:55 -0500

    moved things around in server.js to better organize it

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const userRouter = require("./users/userRouter");
 
 const server = express();
 server.use(express.json());
+server.use("/api/users/", userRouter);
 
 //custom middleware
 
@@ -10,8 +11,8 @@ const logger = (request, response, next) => {
   console.log(`[${new Date().toISOString()}] ${request.method} to ${request.url} ${request.get("Origin")}`)
   next();
 }
-
 server.use(logger)
+
 
 server.get('/', (request, response) => {
   response.send(`<h2>Let's write some middleware!</h2>`);
