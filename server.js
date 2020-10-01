6083e0ea97a7306170ecf297d2ec029c7124f8d6
victor dronov <victordronov1@gmail.com>
2020-10-01 10:27:03 -0400

    deployed on heroku

--- server.js
+++ server.js
@@ -17,7 +17,15 @@ server.use(express.json());
 server.use(logger);
 
 server.get("/", (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(
+    `
+    <h2>End Points</h2>
+    <p>GET /users</p><span>Gets a list of users</span>
+    <p>GET /posts</p><span>Gets a list of posts</span>
+    <p>GET /users/:id/posts</p><span>Gets a list of posts for that user</span>
+    <p>POST /users</p><span>Creates a user Requires a name to be in the body</span>
+    `
+  );
 });
 
 module.exports = server;
