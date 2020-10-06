84e25e3b44a3d1446fdb3146ca20eba21317566e
EmilyLow <emlow@me.com>
2020-10-05 20:29:17 -0500

    Running on 4000

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+const server = require("./server.js");
+
+server.listen(4000, () => {
+  console.log("Server listening on port 4000...");
+});
+
+//Why do we have both index.js and server.js here? Is it because you can only put the location in index.js?
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+
+
 const server = express();
 
 server.get('/', (req, res) => {
