05857f483d7c7770ef87075b844d035de04cf345
noahbibb21 <noahbibb90@gmail.com>
2020-10-07 16:27:34 -0400

    should be  mvp

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
-
 const server = express();
 
+const userRouter = require("./users/userRouter")
+const postRouter = require("./posts/postRouter")
+
+server.use(express.json())
+server.use("/api/users", logger, userRouter)
+server.use("/api/posts", logger, postRouter)
+
 server.get('/', (req, res) => {
+  const message = process.env.MESSAGE
+  res.status(200).json({message})
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`
+  );
+  next();
+}
 
 module.exports = server;
