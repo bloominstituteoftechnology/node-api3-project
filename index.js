7894fe612362fda75ed5049a8d638815310c274f
pvahanian <51976194+pvahanian@users.noreply.github.com>
2020-10-28 20:09:45 -0500

    Ziggs mvp plus stretch

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code away!
+const server = require('./server.js');
+
+server.listen(4500, () => {
+  console.log('\n* Server Running on http://localhost:4500 *\n');
+});
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,70 @@
 const express = require('express');
 
+const Posts = require('./postDb')
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Posts.get()
+  .then((posts) => {
+    // console.log(posts)
+    res.status(200).json(posts)
+  })  
+  .catch((error) => {
+    console.log(error)
+    res.status(500).json({
+      message: "Unable to retrieve posts"
+    })
+  })
+
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, (req, res) => {
+  // Posts.getById(req.params.id)
+  // .then((posts) => {
+    // console.log(req)
+    res.status(200).json(req.post)
+  // })  
+  // .catch((error) => {
+  //   console.log(error)
+  //   res.status(500).json({
+  //     message: "Unable to retrieve posts"
+  //   })
+  // })
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validatePostId, (req, res) => {
+  Posts.remove(req.params.id)
+  .then(response => {res.status(200).json({message:'The post has been deleted'})})
+
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId, (req, res) => {
+  Posts.update(req.params.id, req.body)
+  .then(response => {
+    console.log("this is my response", response)
+    res.status(200).json({message:'The post has been updated'}
+    )})
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const { id } = req.params
+  Posts.getById(id)
+  .then(post =>{
+    // console.log("this is going through validator",post)
+    if(post){
+      req.hub = post
+      next()
+    }
+    else
+    {
+      res.status(404).json({
+        message:"Error loading post, cant find that id"
+      })
+    }
+  })
 }
 
 module.exports = router;
