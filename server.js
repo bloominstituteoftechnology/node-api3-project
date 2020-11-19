8564ea20d5df64e18400296015e70a7c41c6ffe1
unknown <brettmcadams96@gmail.com>
2020-11-19 15:25:18 -0500

    mvp

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
+const userRouter = require('./users/userRouter')
+
 const server = express();
+
+server.use(express.json()); // the req now has a body object
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +13,11 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.method, req.url, new Date);
+  next()
+}
+
+server.use('/user', userRouter)
 
 module.exports = server;
