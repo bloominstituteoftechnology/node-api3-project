48ea9bc3da52662b45f39068b4c6a83aa4cd65c1
LBU <lbu@LBUui-MacBook-Pro.local>
2020-12-02 18:05:28 -0500

    post routing done

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -14,22 +14,62 @@ router.get('/', (req, res) => {
     })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, (req, res) => {
+  const { id } = req.params
+  Post.getById(id)
+  .then(post =>  {
+    res.status(200).json(post)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({ error: err.message})
+  })
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  const { id } = req.params
+  Post.remove(id)
+    .then(post => {
+      res.status(200).json({ message :  'post deleted'})
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error : err.message })
+    })
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  const { id } = req.params
+  const changes = req.body
+
+  Post.update(id, changes)
+    .then(post => {
+      res.status(201).json(changes)
+    })
+    .catch(err  => {
+      console.log(err)
+      res.status(500).json({ error : err.message })
+    })
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
   const { id } =  req.params
+  Post.getById(id)
+  .then(postId => {
+    if(postId){
+      req.id = id
+      next()
+    }
+    else{
+      res.status(404).json({ message: `post with id ${id} not found`})
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({error: err.message})
+  })
   
 }
 
