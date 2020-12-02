ff39c5d0bb4b65b90cc4d48ff851d586ad01595f
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-02 16:56:55 -0600

    validateUserId FINALLY WORKING - don't forget your awaits homie

--- users/userRouter.js
+++ users/userRouter.js
@@ -4,6 +4,31 @@ const User = require('./userDb');
 
 const router = express.Router();
 
+//custom middleware ---------------------------
+ const validateUserId = async (req, res, next) => {
+  const { id } = req.params; 
+  try {
+    const user = await User.getById(id); 
+    if (!user) { 
+      res.status(404).json({ message: `User with id of ${id} not found`});
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error retrieving user' });
+  }
+}
+
+function validateUser(req, res, next) {
+  // do your magic!
+}
+
+function validatePost(req, res, next) {
+  // do your magic!
+}
+
+//JUICY ENPOINTS ---------------------------------
 router.post('/', (req, res) => {
   // do your magic!
 });
@@ -21,8 +46,15 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId, async (req, res) => {
+  const { id } = req.params;
+
+  try { 
+    const user = await User.getById(id); 
+    res.status(200).json(user); 
+  } catch (error) {
+    res.status(500).json({ message: error.message }); 
+  }
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -37,18 +69,6 @@ router.put('/:id', (req, res) => {
   // do your magic!
 });
 
-//custom middleware
 
-function validateUserId(req, res, next) {
-  // do your magic!
-}
-
-function validateUser(req, res, next) {
-  // do your magic!
-}
-
-function validatePost(req, res, next) {
-  // do your magic!
-}
 
 module.exports = router;
