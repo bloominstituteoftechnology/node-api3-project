aceed8844f851544251dda9a6c07d406e8654503
will Fletcher <wfletch7@gmail.com>
2020-12-02 16:20:55 -0500

    finished MVP and stretch

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,80 @@
 const express = require('express');
-
+const Posts = require('./postDb')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  const{query} = req
+
+  Posts.get(query)
+  .then(posts => {
+    res.status(200).json(posts)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({error: err.message})
+  })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, (req, res) => {
+  const {id} = req.params
+
+  Posts.getById(id)
+  .then(post => {
+    res.status(200).json(post)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({error: err.message})
+  })
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validatePostId, (req, res) => {
+  const { id } = req.params
+
+  Posts.remove(id)
+  .then(del => {
+    res.status(200).json({message:'wow that was mean :O'})
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({error: err.message})
+  })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId, (req, res) => {
+  const {id} = req.params
+  const changes = req.body
+
+  Posts.update(id, changes)
+  .then(post => {
+    res.status(200).json(changes)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({error: err.message})
+  })
+
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const { id } = req.params;
+
+  users.getById(id)
+  .then(postID => {
+    if(postID){
+      req.id = id;
+      next();
+    } else{
+      res.status(404).json({ message: `post with id ${id} not found` })
+    }
+    
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({error: err.message})
+  })
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const express = require('express');
 
 const server = express();
 
+const userRouter = require('./users/userRouter')
+const postRouter = require('./posts/postRouter')
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+
+server.use(express.json())
+server.use('/api/users', userRouter)
+server.use('/api/posts', postRouter)
+server.use(logger)
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.url, req.method)
+
+
+}
 
 module.exports = server;
