e25527cc13eebdc526e4eeb4d33678fea9418a5c
DiamondDyer <diamond.l.dyer@gmail.com>
2020-12-03 07:05:50 -0500

    project

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,71 @@
 const express = require('express');
-
+const postsData = require('./postDb');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  postsData
+    .get(req)
+    .then((res) => {
+    res.status(200).json({data: res})
+    })
+    .catch((err) => {
+    res.status(500).json({message: 'Oops, failed to retrieve posts!'})
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  postsData
+    .getById(req.params.id)
+    .then((res) => {
+    res.status(200).json({data: res})
+    })
+    .catch((err) => {
+    res.status(500).json({message: `Oops, can not locate post with this id ${id}`})
+  })
 });
+
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  postsData
+    .remove(req.params.id)
+    .then((res) => {
+      if (res) {
+        res.status(200).json({ Message: "Post removed!" });
+      } else {
+        res.status(404).json({ Message: "Post not found" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ Message: "Sorry, message not found!" });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+
+router.put("/:id", (req, res) => {
+  const changes = req.body
+  console.log(req.body)
+  postsData
+    .update(req.params.id, changes)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ Message: "Post could not be found" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ Message: "Server could not find this post" });
+    });
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  if (req.params.id) {
+    next();
+  } else {
+    res.status(400).json({ message: "Invalid post id!" });
+  }
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
-
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`On this date,[${Date()}] a ${req.method} request was sent to ${req.url}`);
+  req.body = "name";
+  next();
+}
+
+server.use("/posts", postRouter);
+server.use("/users", userRouter);
+server.use(express.json());
+server.use(logger);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
 
 module.exports = server;
