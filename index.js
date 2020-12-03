fdae587c93611c5dfa141557c03c3ac4597a88d1
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-03 15:10:23 -0600

    added stuff for heroku

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // code away!
 // Import our server 
+const dotenv = require('dotenv').config();
+
 const server = require('./server'); 
 
+const port = process.env.PORT || 4000;
+
 //Set server to listen on our local port of 4000 
-server.listen(4000, () => {
-    console.log('\n* Server Running on http://localhost:4000 *\n');
+server.listen(port, () => {
+    console.log(`\n* Listening on ${port} *\n`);
 });
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+
 const express = require('express'); 
+const cors = require('cors');
+
+
 const usersRouter = require('./users/userRouter'); 
 const postsRouter = require('./posts/postRouter');
 
 
 const server = express();
 //not custom middleware 
+server.use(cors());
 server.use(express.json()); 
 //custom middleware
 
