21833a25ab436526c91d6403efbcfe1a534bcf42
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-13 17:25:21 -0500

    /post router and middleware set up and functioning

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,5 @@
+const Posts = require("../posts/posts-model");
+
 function logger(req, res, next) {
   // do your magic!
 }
@@ -11,11 +13,41 @@ function validateUser(req, res, next) {
 }
 
 function validatePostId(req, res, next) {
+  const { id } = req.params;
+  Posts.getById(id)
+    .then((post) => {
+      if (!post) {
+        res.status(404).json({ message: `Post with ID ${id} does not exist` });
+      } else {
+        req.post = post;
+        next();
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ err: error, message: "Internal server error" });
+    });
   // do your magic!
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  const { text, user_id } = req.body;
+  if (!text || !user_id) {
+    res.status(400).json({ message: "Please include text and user_id" });
+  } else {
+    Posts.insert(req.body)
+      .then((post) => {
+        req.newPost = post;
+        next();
+      })
+      .catch((error) => {
+        res.status(500).json({ error: "There was an error inserting post" });
+      });
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  validatePostId,
+  validatePost,
+};
