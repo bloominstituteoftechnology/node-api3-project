1931ef86f2acfdfd7f211aa32c7db7c6f0a8a29b
cmirza <mail@cmirza.com>
2021-01-13 20:47:33 -0800

    mostly done

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,23 +2,50 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
-  // this needs a middleware to check that the request body is valid
+const Users = require('./users-model');
+
+const { validateUserId, validateUser } = require('../middleware/middleware');
+
+router.post('/', validateUser, (req, res) => {
+  Users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Server side error.' });
+    });
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Users.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Server side error.' });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-  // this needs a middleware to verify user id
+router.get('/:id', validateUserId, (req, res) => {
+  const { id } = req.params;
+  Users.getById(id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Server side error.' });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-  // this needs a middleware to verify user id
+router.delete('/:id', validateUserId, (req, res) => {
+  const { id } = req.params;
+  Users.remove(id)
+    .then(() => {
+      res.status(201).json({ message: `User ${id} has been deleted.` });
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Server side error.' });
+    });
 });
 
 router.put('/:id', (req, res) => {
@@ -39,3 +66,4 @@ router.get('/:id/posts', (req, res) => {
 });
 
 // do not forget to export the router
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // require your server and start it
+const server = require('./api/server.js');
+
+server.listen(5000, () => {
+    console.log('Server running on port 5000.')
+});
