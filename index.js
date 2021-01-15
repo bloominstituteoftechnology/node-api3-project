91c41902e7ba226a6aa24f0fe97b84e2d027938a
cmirza <mail@cmirza.com>
2021-01-14 19:41:39 -0800

    changes for Heroku deployment

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(5000, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}.`)
 });
