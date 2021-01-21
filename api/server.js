62a1cca1fc763edf4a319c0b7a579906a0099cf6
Mark Hillin <mark.hillin@gmail.com>
2021-01-21 15:40:42 -0600

    .env added

--- api/server.js
+++ api/server.js
@@ -8,6 +8,8 @@ const {logger} = require('./middleware/middleware');
 const usersRouter = require('./users/users-router');
 const postsRouter = require('./posts/posts-router');
 
+const motd = process.env.MOTD || "hello world"
+
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json());
 // global middlewares and routes need to be connected here
@@ -18,7 +20,7 @@ server.use('./api/users', usersRouter);
 server.use('./api/posts', postsRouter);
 
 server.get('/', logger, (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2>${motd}</h2>`);
 });
 
 module.exports = server;
