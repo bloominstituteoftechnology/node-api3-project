3141a4c82df0b576a6d8c684b7eb3aec12e602a0
KVlearn <kavya.mdu@gmail.com>
2021-01-21 20:33:29 -0800

    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // require your server and start it
 const server=require('./api/server');
 
-const PORT=4000;
-server.listen(PORT,()=>{
-    console.log(`Server up at port ${PORT}`)
+const port= process.env.PORT || 5000;
+server.listen(port,()=>{
+    console.log(`Server up at port ${port}`)
 })
