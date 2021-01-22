fb4856235877a5162d0aeccc9f53e0173d63f1e9
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-01-21 19:47:54 -0800

    Server Added

--- api/server.js
+++ api/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors')
+const postsRouter = require('./posts/posts-router.js')
 
 const server = express();
 
@@ -6,8 +9,27 @@ const server = express();
 
 // global middlewares and routes need to be connected here
 
+
+server.use(express.json())
+server.use(helmet());
+server.use(cors());
+server.use((req,res,next) =>{
+  console.log('Welcome To the Middleware Projects App')
+
+  next();
+
+})
+const postsRouterPipeline = [logger,logger,logger]
+server.use('/api/posts', postsRouterPipeline, postsRouter )
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  const textIn = (req.text) ? ` ${req.text}` : '';
+  res.send(`<h2>Post Says ${textIn}!</h2>`);
 });
 
 module.exports = server;
+
+function logger(req, res, next) {
+  console.log('falling into posts router')
+  next()
+}
