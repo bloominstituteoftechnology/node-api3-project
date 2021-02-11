063aad53f92dd02195b97c6eba307a33701c4db0
ty <tylerpmatthews1998@gmail.com>
2021-02-10 23:37:09 -0500

    MVP

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it
+const server = require('./api/server');
+const PORT = 9999
+
+server.listen(PORT, () => {
+    console.log(`Port: ${PORT} is currently running!`)
+})
