cce67bc1869983bb113436116d7fef2e8352e2be
Brian Choi <b.choi562@gmail.com>
2021-02-11 20:10:45 -0800

    commit

--- index.js
+++ index.js
@@ -2,11 +2,8 @@
 require("dotenv").config();
 const server = require('./api/server');
 
+const port = process.env.PORT || 5000;
 
-// const PORT = process.env.PORT || 9000;
-
-// server.listen(PORT, () => {
-//     console.log(`\n* Server Running on ${PORT} *\n`);
-// });
-
-server.listen(process.env.PORT || 5000);
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`)
+})
