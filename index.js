550d7632cfdd2ee051357cbda818561ecfa187ad
Brian Choi <b.choi562@gmail.com>
2021-02-11 20:38:01 -0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // require your server and launch it
-require("dotenv").config();
+const dotenv = require("dotenv").config();
 const server = require('./api/server');
 
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
     console.log(`\n*** Server Running on http://localhost:${port} ***\n`)
 })
