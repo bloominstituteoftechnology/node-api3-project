b1744e4eb3366f48b0d6cad2d440ca624e914072
ndossett <niki.m.dossett@gmail.com>
2021-02-11 22:17:37 -0500

    finished users-router

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,61 @@
-function logger(req, res, next) {
+const Posts = require('../posts/posts-model');
+const Users = require('../users/users-model');
+
+function logger (req, res, next) => {
   // do your magic!
+  console.log(req.method, 'Request URL:', req.originalUrl, new Date().toUTCString())
 }
 
-function validateUserId(req, res, next) {
-  // do your magic!
+async function validateUserId(req, res, next) {
+  const { id } = req.params
+  const user = await Users.getById(id)
+
+  try {
+    if(!user){
+      res.status(404).json({
+        message: 'User not found'
+      })
+    } else {
+      req.id = id
+      req.user = user
+      next()
+    }
+  } catch (error) {
+    res.status(404).json(error.message)
+  }
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  const { name } = req.body
+
+  if(!name) {
+    res.stautus(400).json({
+      message: 'name is required'
+    })
+  } else {
+    req.name = name
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  const { text } = req.body
+
+  if (!text) {
+    res.status(400).json({
+      message: 'missing required text field'
+    })
+  } else {
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,41 +1,90 @@
 const express = require('express');
 
+const Users = require('../users/users-model')
+const mw = require('../middleware/middleware')
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', mw.logger, async (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  try {
+    const users = await Users.get();
+    res.status(200).json(users)
+  } catch(error){
+    res.status(404).status(error.message)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', mw.validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.status(200).json(req.user)
 });
 
-router.post('/', (req, res) => {
+router.post('/', mw.validatePost, async (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  try {
+    const user = await Users.insert(req.body)
+
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(404).status(error.message)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', mw.validateUserId, async (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+
+  try{
+    const postUpdate = await Users.update(req.id, req.body)
+
+    res.status(200).json(postUpdate)
+  }  catch (error) {
+    res.status(404).json(error.message)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', mw.validateUserId, async (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  try {
+
+    const postRemove = await Users.remove(req.id)
+
+    res.status(200).json(postRemove)
+
+  } catch (error) {
+    res.status(404).json(error.message)
+  }
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', mw.validateUserId, async (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  try {
+    const getPost = await Users.getUserPosts(req.id)
+
+    res.status(200).json(getPost)
+  } catch (error){
+    res.status(404).json(error.message)
+  }
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', mw.validateUserId, mw.validatePost, async (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  try {
+    const user = await Users.insert({text: req.body, user_id: req.params})
+
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(404).status(error.message)
+  }
 });
 
 // do not forget to export the router
+module.exports = router;
