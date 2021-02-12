c99b8005badd0cf53e5814385dfd33a5e2edf0ce
ty <tylerpmatthews1998@gmail.com>
2021-02-11 20:59:46 -0500

    ready for heroku

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // require your server and launch it
-const server = require('./api/server');
-const PORT = 9999
+const server = require("./api/server");
+const dotenv = require("dotenv");
+const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
-    console.log(`Port: ${PORT} is currently running!`)
-})
+  console.log(`Port: ${PORT} is currently running!`);
+});
