f3908aaff7df1fe599caf3834a1b0aaa573d474f
amotor-AM <66324211+amotor-AM@users.noreply.github.com>
2021-02-16 18:34:12 -0800

    Completed Assignment
    Completed MVP

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,80 @@
+const users = require("../users/users-model")
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(`Method: ${req.method}, URL: ${req.url}, Timestamp: ${Date().toISOString()}`)
+  next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+function validateUserId() {
+  return (req, res, next) => {
+    users.getById(req.params.id)
+      .then ((user) => {
+        if (user) {
+          req.user = user;
+          next();
+        } else {
+          res.status(404).json({message: "user not found"});
+        }
+      })
+      .catch ((error) => {
+        console.log(error);
+      })
+  }
 }
 
-function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+
+// function validateUser(req, res, next) {
+//   if(!req.body) {
+//     res.status(400).json({ message: "missing user data" })
+//   } else if(!req.body.name) {
+//     res.status(400).json({ message: "missing required name field" })
+//   } else {
+//     req.userEdit = req.body
+//     next()
+//   }
+// }
+
+function validateUser() {
+  return (req, res, next) => {
+  if(!req.body) {
+    res.status(400).json({ message: "missing user data" })
+  } else if(!req.body.name) {
+    res.status(400).json({ message: "missing required name field" })
+  } else {
+    next()
+  }
+}
 }
 
-function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+// function validatePost(req, res, next) {
+//   if(!req.body) {
+//     res.status(400).json({ message: "missing post data" })
+//   } else if(!req.body.text) {
+//     res.status(400).json({ message: "missing required text field" })
+//   } else {
+//     res.postEdit = req.body
+//     next()
+//   }
+// }
+
+function validatePost() {
+  return (req, res, next) => {
+    if(!req.body) {
+      res.status(400).json({ message: "missing post data" })
+    } else if(!req.body.text) {
+      res.status(400).json({ message: "missing required text field" })
+    } else {
+      res.postEdit = req.body
+      next()
+    }
+  }
+}
+
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
 }
 
 // do not forget to expose these functions to other modules
--- api/server.js
+++ api/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
-
+const userRouter  = require("./users/users-router")
+const {logger} = require("./middleware/middleware")
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
+server.use(logger)
+server.use(express.json())
+server.use(userRouter)
 
-// global middlewares and routes need to be connected here
-
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+server.use((err, req, res, next) => {
+  console.log(err) 
+  res.status(500).json({
+      message: "Something went wrong, please try again later"
+  })
+})
 
 module.exports = server;
