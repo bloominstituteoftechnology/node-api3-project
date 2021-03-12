cb53ea3a8ce873be363e930d4f1198d8fbbb7396
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2021-03-12 00:39:50 -0800

    POST a new user object crashes server, everything else works but still need to run test file as well

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,23 +1,60 @@
+const Users = require('../users/users-model');
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
-  const {method,url, timestamp} = req
+  const {method,url} = req
   console.log({
     method: method,
     url: url,
-    timestamp: timestamp
-  })
+    timestamp: new Date().toUTCString()
+  });
+  next();
 }
 
-function validateUserId(req, res, next) {
+// QUESTION: WHY DO WE USE ASYNC AWAIT HERE?
+const validateUserId = async (req, res, next) => {
   // DO YOUR MAGIC
+  const {id} = req.params;
+  try {
+    const user = await Users.getById(id)
+    if (!user) {
+      res.status(404).json({message: "user not found"})
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+
 }
 
-function validateUser(req, res, next) {
+// Q: where should line 35 go? inside try catch?
+const validateUser = (req, res, next) => {
   // DO YOUR MAGIC
+  const {body} = req
+  try {
+    if (!body) {
+      res.status(400).json({message: "missing user data"})
+    } if (!body.name) {
+      res.status(400).json({message: 'missing required name field'})
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err);
+  }
+  
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  if (!req.body) {
+    res.status(400).json({ message: 'missing post data' });
+  } else if (!req.body.text) {
+    res.status(400).json({ message: 'missing required text field' })
+  } else {
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -11,7 +11,9 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   Users.get()
-    .then((users) => res.status(200).json(users))
+    .then((users) => {
+      res.status(200).json(users)
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json({
@@ -23,6 +25,7 @@ router.get('/', (req, res) => {
 router.get('/:id',validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.status(200).json(req.user)
 });
 
 router.post('/',validatePost, (req, res) => {
@@ -34,22 +37,42 @@ router.put('/:id',validateUserId,validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  Users.update(req.params.id,req.body)
+    .then(res.status(200).json(req.body))
+    .catch(() => {res.status(500).json({message: 'error updating user'})})
 });
 
 router.delete('/:id',validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  Users.remove(req.params.id)
+    .then(res.status(200).json({message: 'successful delete'}))
+    .catch((err) => {
+      res.status(500).json({message: err})
+    })
+  
 });
 
 router.get('/:id/posts',validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  Users.getUserPosts(req.params.id)
+    .then((posts) => {
+      res.status(200).json(posts)
+    }).catch(()=> {
+      res.status(500).json({message: 'error getting user posts'})
+    })
 });
 
 router.post('/:id/posts',validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  Users.insert(req.body)
+    .then(res.status(202).json(req.body))
+    .catch(()=> {
+      res.status(500).json({message: 'error creating new user object'})
+    })
 });
 
 // do not forget to export the router
