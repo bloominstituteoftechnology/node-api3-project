d5c933f006c8da4ae60f959f6cb6137255c96dd3
kato hawkins <kristhahawkins@icloud.com>
2021-03-16 02:55:23 -0400

    still working

--- api/server.js
+++ api/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
-
+const helmet = require('helmet');
+const morgan = require('morgan');//logger
 const server = express();
+const userRouter = require('./users/users-router.js');
+const mw = require("./middleware/middleware.js")
 
 // remember express by default cannot parse JSON in request bodies
 
+server.use(helmet())
+server.use(morgan('dev'))
+server.use(express.json());
 // global middlewares and the user's router need to be connected here
+server.use(userRouter)
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
 
+
+server.use((err, req, res, next) => {
+  console.log(err) 
+  res.status(500).json({
+      message: "Something went wrong, please try again later"
+  })
+})
 module.exports = server;
