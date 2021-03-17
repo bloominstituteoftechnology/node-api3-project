ca3f2b37571542a790d84a04bb8b87d12f0b565b
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-03-17 00:14:10 -0600

    Almost hit mvp, just one sqlite error not resolved.

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,52 @@
+const Users = require('../users/users-model');
+const Posts = require('../posts/posts-model');
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
-}
+  console.log(`Method: ${req.method}, Url: http://${req.hostname}:5000${req.path}, Date and time: ${new Date()}`);
+  next();
+};
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
-}
+  const {id} = req.params;
+
+    const user = await Users.getById(id);
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(404).json({ message: "user not found" });
+    }
+};
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
-}
+  const user = req.body;
+
+  if (!user) {
+    res.status(400).json({ message: "missing user data"});
+  } else if (!user.name) {
+    res.status(400).json({ message: "missing required name field" });
+  } else {
+    console.log(user);
+    next();
+  }
+};
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
-}
+  const post = req.body;
+
+  if (!post) {
+    res.status(400).json({ message: "missing post data" });
+  } else if (!post.text) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    console.log(post);
+    next();
+  }
+};
 
 // do not forget to expose these functions to other modules
+module.exports = {logger, validateUserId, validateUser, validatePost}; 
--- api/server.js
+++ api/server.js
@@ -2,9 +2,19 @@ const express = require('express');
 
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
+const {logger} = require('./middleware/middleware');
+const usersRouter = require('./users/users-router');
 
+// remember express by default cannot parse JSON in request bodies
+server.use(express.json());
 // global middlewares and the user's router need to be connected here
+server.use(logger);
+server.use('/api/users', usersRouter);
+// function logger(req, res, next) {
+//   // DO YOUR MAGIC
+//   console.log(`Method: ${req.method}, Url: http://${req.hostname}:5000${req.path}, Date and time: ${new Date()}`);
+//   next();
+// };
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
