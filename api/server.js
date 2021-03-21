5af9f09390eb08d2d4f070cf7fb10efc063ee13d
bipin-shrestha <iambpn.shrestha@gmail.com>
2021-03-21 16:21:08 -0700

    updating the codes

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const helmet = require('helmet');
 
-const userRouter = require('./users/users-router');
+const userRouter = require('./users/users-router.js');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const server = express();
+const middleware = require('../api/middleware/middleware.js');
 
 
 // remember express by default cannot parse JSON in request bodies
@@ -11,9 +13,13 @@ const server = express();
 // global middlewares and the user's router need to be connected here
 server.use(helmet());
 server.use(express.json());
+server.use(morgan('dev'));
+server.use(middleware.logger);
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
 
 module.exports = server;
