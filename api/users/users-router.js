37a0bbca97e18e888347a4bbb6c94d42f87afdc4
Matthew Ellis <mbellistx@gmail.com>
2021-04-07 21:34:14 -0500

    fixed post /api/users/:id/posts

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -81,7 +81,7 @@ router.get('/:id/posts', validateUserId, (req, res) => {
     })
 });
 
-router.post('/:id/posts', validatePost, validatePost, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
