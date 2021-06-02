2c1fe7a1dd5b6272182c5a848e93cc6c5c7e3d33
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-06-02 17:32:01 -0400

    server using express and connected

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
+
+server.use(express.json());
 
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
