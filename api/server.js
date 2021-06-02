0b09cba76854a70d140e43e78570901be11ef63e
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-06-02 17:05:35 -0400

    Logger works and connected all middleware to server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 
+const usersRouter = require('./users/users-router');
+
 const server = express();
+
+const { 
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost 
+} = require('./middleware/middleware')
 
 // remember express by default cannot parse JSON in request bodies
 
+server.use(express.json());
+
 // global middlewares and the user's router need to be connected here
+
+server.use(logger)
+server.use(validateUserId)
+server.use(validateUser)
+server.use(validatePost)
+
+server.use(usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,10 +3,17 @@ const express = require('express');
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
+
 const router = express.Router();
 
+const User = require('./users-model');
+// const Post = require('../posts/posts-model');
+// const {
+//   logger
+// } = require('../middleware/middleware');
+
 router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+
 });
 
 router.get('/:id', (req, res) => {
@@ -42,3 +49,4 @@ router.post('/:id/posts', (req, res) => {
 });
 
 // do not forget to export the router
+module.exports = router;
