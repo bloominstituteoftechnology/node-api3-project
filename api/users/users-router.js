759c9a75192b29fefb8a44e03bba8489f5692622
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-06-02 23:27:00 -0400

    DELETE is working

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -41,10 +41,13 @@ router.put('/:id', validateUserId, validateUser, (req, res, next) => {
     .catch(next)
 });
 
-router.delete('/:id', validateUserId, (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
-  console.log(req.user)
+router.delete('/:id', validateUserId, async (req, res, next) => {
+  try {
+    await User.remove(req.params.id)
+    res.json(req.user)
+  } catch (err) {
+    next(err)
+  }
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
