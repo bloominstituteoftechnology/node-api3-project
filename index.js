13731318b370777f361aecd1133452d6a48c93c2
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-06-03 13:13:43 -0400

    Test Message

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const server  = require('./api/server')
 
 const port = process.env.PORT || 5000
 
-
+server.get('/api', (req,res)=>{
+    res.json({message:'Working Project!'})
+})
 // START YOUR SERVER HERE
 server.listen(port,()=>{
     console.log('listening on', port);
