81fdc303d4531888724257225649730a9b787f72
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-06-02 23:32:40 -0400

    GET posts is working for an individual user

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -50,10 +50,13 @@ router.delete('/:id', validateUserId, async (req, res, next) => {
   }
 });
 
-router.get('/:id/posts', validateUserId, (req, res) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
-  console.log(req.user)
+router.get('/:id/posts', validateUserId, async (req, res, next) => {
+  try {
+    const result = await User.getUserPosts(req.params.id)
+    res.json(result)
+  } catch (err) {
+    next(err)
+  }
 });
 
 router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
