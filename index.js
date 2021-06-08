3cb31bb9f646e1de550c9d4ae38dd6acdd047718
yonathan Admasu <admyoni4@yahoo.com>
2021-06-08 16:45:06 -0700

    worked on the project and finished

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it
+const server = require('./api/server')
+const port = `\n* http://localhost:5000*\n`
+
+server.listen(port, () => {
+    console.log(`server running on ${port}`);
+});
+
+
