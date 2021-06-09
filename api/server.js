da5547af9151c562023a07b714aca842a42cae01
artofmayhem <chef_tony_miller@yahoo.com>
2021-06-08 20:29:30 -1000

    sick with flu will finish tomorrow morning

--- api/server.js
+++ api/server.js
@@ -1,13 +1,48 @@
 const express = require('express');
 
 const server = express();
+function logger(req, res, next) {
+  console.log(
+      `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+          'Origin'
+      )}`
+  );
+
+  next();
+}
+
+// function atGate(req, res, next) {
+//   console.log("hit the auth gate");
+//   next()
+// }
+
+// function auth(req, res, next) {
+//   if (req.url === '/' ){
+//     next()
+//   } else {
+//     res,send('Not Allowed')
+//   }
+// }
+
 
 // remember express by default cannot parse JSON in request bodies
+server.use(express.json())
+server.use(logger)
 
-// global middlewares and the user's router need to be connected here
+// global middleware and the user's router need to be connected here
+
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+
+server.use(function(req, res) {
+  res.status(404).send(`<h1>Oops... you've stepped in the void!! This page is 404 NOT FOUND</h1>`)
+});
+
+
+
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it
+const server = require('./api/server');
+
+const port = 5000;
+const message = "Server Running: localhost:5000"
+// START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log(message)
+})
