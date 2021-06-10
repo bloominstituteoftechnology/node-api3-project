2f45a822d330dbf64ea6d02e140b46011d0ea395
artofmayhem <chef_tony_miller@yahoo.com>
2021-06-09 17:47:10 -1000

    mvp reached

--- api/server.js
+++ api/server.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const server = express();
 const userRouter = require('./users/users-router');
-const postsRouter = require('./posts/posts-router');
 const {
-  logger,
-  // validateUserId,
-  // validateUser,
-  // validatePost,
+    logger
 } = require('./middleware/middleware');
 
 //json parser
 server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
-server.use( logger);
-server.use( '/api/users', userRouter )
-server.use( logger );
-server.use( '/api/posts', postsRouter )
+server.use(logger);
+server.use('/api/users', userRouter)
+
+
 
 //server set up
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+    res.send(`<h2>Let's write some middleware!</h2>`);
 });
 module.exports = server;
