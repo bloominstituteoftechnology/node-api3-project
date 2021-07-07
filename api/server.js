082c8074641aa41811bd401efe6b6338d77ed167
Karla Nieves <karlaxnieves@gmail.com>
2021-07-07 15:08:18 -0700

    got started on the index

--- api/server.js
+++ api/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
-
 const server = express();
 
+// const {
+//   logger,
+//   validateUserId,
+//   validateUser,
+//   validatePost
+// } = require('./middleware/middleware')
+
 // remember express by default cannot parse JSON in request bodies
+server.use(express.json())
 
 // global middlewares and the user's router need to be connected here
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
