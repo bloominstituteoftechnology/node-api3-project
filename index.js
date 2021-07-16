28ed07d7703bf4f949e36d562a32bc915f37fbe5
Stan T <stan@Stans-Mac-mini.local>
2021-07-15 20:09:38 -0700

    back to origin

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = require("./api/server");
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-  console.log("\n*** Server running on ${port} ***\n");
+  console.log(`\n*** Server running on ${port} ***\n`);
 });
