6638d35c0ee6063c0f74bc70d9393c036849b58b
nicholastorr <nick.torres213@gmail.com>
2021-09-14 23:07:06 -0400

    mvp complete

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,55 @@
+const Users = require('../users/users-model')
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(`${req.method} ${req.path} ${Date.now()}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  
+
+    try{
+      const {id} = req.params;
+      const user = Users.findById(id);
+      if(!user){
+         res.status(404).json({message: `User ${user} not found`});
+      } else {
+        req.user = user;
+        next();
+      } 
+    } catch(err){
+      res.status(500).json({message: err.message});
+    }
+    
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const { name } = req.body;
+  if(!name || !name.trim()){
+    res.status(400).json({message: 'Missing name'});
+  } else {
+    req.name = name.trim()
+    next();
+  }
 }
 
+
+
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  const { text } = req.body;
+  if(!text || !text.trim()){
+    res.status(400).json({message: 'Missing text'});
+  } else {
+    req.text = text.trim()
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+};
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it
+const server = require('./api/server');
+
+server.listen(process.env.PORT || 4000, () => {
+    console.log(`\n*** Server is running on port 4000 ***\n`);
+})
+
+
+
