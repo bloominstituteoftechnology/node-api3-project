cc226c738dc9dbf89997454a0181ae39f704c70f
Ryan Mark <rymark907@gmail.com>
2021-10-27 13:22:56 -0700

    Able to delete users from API

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -38,9 +38,13 @@ router.put('/:id', validateUserId, validateUser, async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
+router.delete('/:id', validateUserId, async (req, res) => {
+  try {
+    const deletedUser = await Users.remove(req.params.id);
+    res.status(200).json(deletedUser);
+  } catch (err) {
+    res.status(500).json({ message: "something went wrong" })
+  }
 });
 
 router.get('/:id/posts', (req, res) => {
