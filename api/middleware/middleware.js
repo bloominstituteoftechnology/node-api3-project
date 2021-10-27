d929a5377b4c15427c42d756c0ebe4b92e31292c
Ryan Mark <rymark907@gmail.com>
2021-10-27 12:44:55 -0700

    middleware for validating userid complete

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,11 +1,24 @@
+const Users = require('../users/users-model');
+
+
 function logger(req, res, next) {
   console.log(`Time Stamp: ${new Date().toISOString()}, Request Method: ${req.method}, Request URL: ${req.url}`);
 
   next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const potentialUser = await Users.getById(req.params.id)
+    if (!potentialUser) {
+      res.status(404).json({ message: "user not found"})
+    } else {
+      req.user = potentialUser;
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong" })
+  }
 }
 
 function validateUser(req, res, next) {
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,16 +3,21 @@ const Users = require('./users-model');
 const Posts = require('../posts/posts-model');
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
+const { validateUserId } = require('../middleware/middleware');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+router.get('/', async (req, res) => {
+  try {
+    const users = await Users.get()
+    res.status(200).json(users)
+  } catch (err) {
+    res.status(500).json({ message: "something went wrong" })
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(req.user);
 });
 
 router.post('/', (req, res) => {
