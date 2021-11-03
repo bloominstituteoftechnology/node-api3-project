5bfa5ee3d87f81ae57ccaf533891248a69938d65
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-11-02 19:26:16 -0700

    Server is up and running"

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json())
+
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require('./api/server')
+
+const port = 8000
+
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+})
