cd3c025f4858411ed6a849996a7b257ca02e0fbd
Bri <brittanymorris@MacBook-Pro.local>
2021-11-05 17:21:49 -0600

    Added middleware methods.

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,58 @@
+const { getById, getUserPosts } = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(req.method, req.url, Date.now())
+  next
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  getById(req.params.id)
+    .then(user => {
+      if (user) {
+        next()
+      } else {
+        next({
+          status: 404,
+          message: 'user not found'
+        })
+      }
+    })
+    .catch(next)
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  getById(req.params.id)
+  .then(user => {
+    if (user) {
+      next()
+    } else {
+      next({
+        status: 404,
+        message: 'user not found'
+      })
+    }
+  })
+  .catch(next)
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  getUserPosts(req.params.id)
+  .then(user => {
+    if (user) {
+      next()
+    } else {
+      next({
+        status: 404,
+        message: 'user not found'
+      })
+    }
+  })
+  .catch(next)
 }
 
 // do not forget to expose these functions to other modules
+module.exports = { logger, validateUserId, validateUser, validatePost }
