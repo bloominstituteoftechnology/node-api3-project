d225a1e63a19616f355ba1ec8849fcdfa560212b
unknown <jlarson129@outlook.com>
2021-12-07 15:17:44 -0500

    get users, get user by id, post user endpoints

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,22 +1,47 @@
 const express = require('express');
-
-// You will need `users-model.js` and `posts-model.js` both
-// The middleware functions also need to be required
+const mw = require('../middleware/middleware')
+const Posts = require('../posts/posts-model');
+const Users = require('./users-model');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+  Users.get()
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving users',
+    });
+  });
 });
 
-router.get('/:id', (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
+router.get('/:id',mw.validateUserId, (req, res) => {
+  Users.getById(req.params.id)
+    .then(user =>{
+      res.status(200).json(user)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving user',
+      });
+    })
 });
 
-router.post('/', (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+router.post('/',mw.validateUser, (req, res) => {
+  Users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'Error creating user',
+      });
+    })
 });
 
 router.put('/:id', (req, res) => {
