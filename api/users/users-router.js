4c860e113bb1574d75592683a31c9418a5df9147
jose-mercado <josefmercado99@gmail.com>
2021-12-07 23:34:28 -0800

    done

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,9 @@
 const express = require('express');
-
+const Users = require('./users-model');
+const {
+  validateUserId,
+  validateUser,
+  validatePost} = require('../middleware/middleware');
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
@@ -7,38 +11,72 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  Users.get()
+  .then(user => {
+    res.status(200).json(user)
+  })
+  .catch(err => {
+    res.status(500).json({message: err.message})
+  })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.status(200).json(req.user)
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, async (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  try{
+    const newUser = await Users.insert(req.body)
+    res.status(201).json(newUser)
+  }catch (err){
+    res.status(500).json({message: 'Something went wrong'})
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUser,  validateUserId, async (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  const changes = req.body
+  const {id} = req.params
+  const updatedUser = await Users.update(id, changes)
+  res.status(201).json(updatedUser)
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, async (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  try{
+    const user = await Users.getById(req.params.id)
+    res.status(200).json(user)
+  }catch(err){
+    res.status(500).json({message: 'something went wrong'})
+  }
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  const {id} = req.params
+  Users.getUserPosts(id)
+  .then(messages => {
+    res.status(200).json(messages)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({message: 'Error getting the posts for the user'})
+  })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
 // do not forget to export the router
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // require your server and launch it
+const server = require('./api/server')
+
+server.listen(9000, () => {
+    console.log('*** Server listening on port 9000 ***')
+})
