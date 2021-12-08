e3d6f53e0e8e197e19bef06585c8fffc2a4bc979
Aidan Jenkins <aidan.jenkins@gmail.com>
2021-12-07 16:26:57 -0800

    complete mvp

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,49 @@
+const User = require('../users/users-model.js')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(req.url, req.method)
+  next()
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  try {
+    const { id } = req.params
+    const user = await User.getById(id)
+    if(!user){
+      res.status(404).json("user not found")
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message})
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  if(!req.body.name){
+    res.status(400).json("missing required name field")
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  if(!req.body.text){
+    res.status(400).json("missing required text field")
+  } else {
+    next()
+  }
 }
 
+module.exports = {
+  logger, 
+  validateUserId,
+  validateUser,
+  validatePost
+}  
 // do not forget to expose these functions to other modules
