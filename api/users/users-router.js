a198e3c26dff551d3c699001dac68c0426637fcb
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-12-08 16:12:24 -0800

    router.get endpoint functioning

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,3 +1,4 @@
+// The middleware functions also need to be required
 const express = require('express');
 const {
  validateUserId,
@@ -6,12 +7,18 @@ const {
 }=require('../middleware/middleware')
 
 // You will need `users-model.js` and `posts-model.js` both
-// The middleware functions also need to be required
+const User = require("./users-model")
+const Post = require('../posts/posts-model')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  User.get()
+  .then(users=>{
+    res.json(users)
+  })
+  .catch(next)
 });
 
 router.get('/:id', validateUserId, (req, res) => {
@@ -55,6 +62,14 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   console.log(req.user)
   console.log(req.text)
 });
+
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    customMessafe: 'something bad happened',
+    message: err.message,
+    stack: err.stack,
+  })
+})
 
 // do not forget to export the router
 module.exports = router
