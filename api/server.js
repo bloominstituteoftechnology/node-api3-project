f0d2f660682f1c4e6123c12754df7ff427b0cb1e
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-12-09 21:38:30 -0600

    first commit

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-
+server.use(express.json())
 // global middlewares and the user's router need to be connected here
 
 server.get('/', (req, res) => {
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require("./api/server.js")
+
+const port = 9000
+
+server.listen(port, () =>{
+	console.log(`Server is running on http://localhost:${port}`)
+}) 
