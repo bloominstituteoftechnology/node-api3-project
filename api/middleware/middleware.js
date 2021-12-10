fcc2305e3f59257c953c93a71ab6fc4fe3a6f6e6
Lirong Cui <lirongcoding@gmail.com>
2021-12-10 10:47:25 -0800

    setup

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,31 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`[${timestamp}] ${method} to ${url}`)
+  next()
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  next()
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  next()
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  next()
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it
+const server = require('./api/server')
+
+const port = 3000
+
+server.listen(port, () => {
+    
+    console.log('listening on', port)
+})
