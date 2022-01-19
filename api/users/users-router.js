b0697a6d4087d10a52bee1c64693a4e367f5cc85
Cynthia Coronado <cynthia.softball12@gmail.com>
2022-01-18 17:11:31 -0800

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -34,26 +34,55 @@ router.post('/', validateUser, (req, res, next) => {
   .catch(next)
 });
 
-router.put('/:id', validateUserId, validateUser, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  User.update(req.params.id, { name: req.name })
+  .then(() => {
+    return User.getById(req.params.id)
+  })
+  .then(user => {
+    res.status(201).json(user)
+  })
+  .catch(next)
 });
 
-router.delete('/:id', validateUserId, (req, res) => {
+router.delete('/:id', validateUserId, async (req, res, next) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  try{
+    await User.remove(req.params.id)
+    res.json(req.user)
+  }catch(err){
+    next(err)
+  }
 });
 
-router.get('/:id/posts', validateUserId, (req, res) => {
+router.get('/:id/posts', validateUserId, async (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
-});
+  try{
+    const posts = await User.getUserPosts(req.params.id)
+    res.json(posts)
+  }catch(err){
+    next(err)
+  }
+}); 
 
-router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, async (req, res, next) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  try{
+    const newPost = await Post.insert({
+      user_id: req.params.id,
+      text: req.text,
+    })
+    res.status(201).json(newPost)
+  }catch (err){
+    next(err)
+  }
 });
 
 router.use((err, req, res, next) => {
